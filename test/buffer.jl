9a5f1d72e1cb2442eabee2d26df240aee24f3de9
Johanni Brea <jbrea@users.noreply.github.com>
2018-08-21 21:22:36 +0200

    fix test errors

--- test/buffer.jl
+++ test/buffer.jl
@@ -7,13 +7,13 @@ end
 preprocessstate(p::MyPreprocessor, s) = reshape(Int64[s == i for i in 1:p.N], p.N, 1)
 function testbuffers()
     a = ArrayCircularBuffer(Array, Int64, (1), 8)
-    for i in 1:5 push!(a, i) end
+    for i in 1:5 push!(a, [i]) end
     @test a[1] == [1]
     @test a[lastindex(a)] == [5]
     @test lastindex(a) == 5
     @test nmarkovgetindex(a, 5, 3)[:] == collect(3:5)
     @test nmarkovgetindex(a, lastindex(a), 4)[:] == collect(2:5)
-    for i in 6:12 push!(a, i) end
+    for i in 6:12 push!(a, [i]) end
     @test a[1] == [5]
     @test a[lastindex(a)] == [12]
     @test nmarkovgetindex(a, lastindex(a), 4)[:] == collect(9:12)
@@ -36,15 +36,16 @@ function testbuffers()
     for T in [7, 97]
         p = ForcedPolicy(rand(1:4, 100))
         ends = rand(1:100, 10)
-        env = ForcedEpisode(rand(1:10, 100), [i in ends ? true : false for i in 1:100], rand(100))
-        x = RLSetup(1, env, ConstantNumberSteps(98), policy = p, 
+        env = ForcedEpisode([[rand(1:10)] for _ in 1:100], 
+                            [i in ends ? true : false for i in 1:100], rand(100))
+        x = RLSetup(1, env, ConstantNumberSteps(T), policy = p, 
                     buffer = ArrayStateBuffer(capacity = 10, datatype = Int64), 
                     callbacks = [RecordAll()],
                     fillbuffer = true, islearning = false)
         learn!(x)
         @test x.buffer.actions[end-5:end] == x.callbacks[1].actions[end-5:end]
         @test x.buffer.done[end-5:end] == x.callbacks[1].done[end-5:end]
-        @test x.buffer.states[end-5:end][:] == Int64.(x.callbacks[1].states[end-5:end])
+        @test x.buffer.states[end-5:end][:] == vcat(x.callbacks[1].states[end-5:end]...)
         @test x.buffer.rewards[end-4:end] == x.callbacks[1].rewards[end-4:end]
     end
 
@@ -65,9 +66,9 @@ function testbuffers()
     a3 = ArrayCircularBuffer(Array, Int64, (1), 4)
     is = collect(1:9)
     for i in is
-        push!(a1, i)
-        push!(a2, i)
-        push!(a3, i)
+        push!(a1, [i])
+        push!(a2, [i])
+        push!(a3, [i])
     end
     @test nmarkovgetindex(a1, lastindex(a1), 4) == nmarkovgetindex(a2, lastindex(a2), 4) == nmarkovgetindex(a3, lastindex(a3), 4)
 end
