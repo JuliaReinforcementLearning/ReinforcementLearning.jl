215b17ba5f8b7a470c66a811c740ec8b2beaf6c9
Jun Tian <find_my_way@foxmail.com>
2018-09-11 17:39:26 +0800

    sync

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,11 @@ using ReinforcementLearning, Test
 # include("traces.jl")
 # include("epsilongreedypolicies.jl")
 # include("policies.jl")
-
-@testset "tdlearning" begin include("tdlearning.jl") end
+# include("tdlearning.jl")
+# include("losses.jl")
 
 # @testset "mdp solver" begin include("learnmdp.jl") end
-# @testset "learn" begin include("learn.jl") end
 # @testset "linear function approximation" begin include("linfuncapprox.jl") end
 # @testset "preprocessor" begin include("preprocessors.jl") end
 # @testset "monte carlo" begin include("montecarlo.jl") end
 # @testset "small backups" begin include("smallbackups.jl") end
-# @testset "losses" begin include("losses.jl") end
