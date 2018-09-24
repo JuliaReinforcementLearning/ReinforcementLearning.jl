4d8d9a7e20bc4a872723bcb903c580116f0e341f
Johanni Brea <jbrea@users.noreply.github.com>
2018-09-24 10:08:56 +0200

    new version of CircularArrayBuffer

--- src/buffers.jl
+++ src/buffers.jl
@@ -80,6 +80,110 @@ mutable struct ArrayCircularBuffer{T}
     counter::Int64
     full::Bool
 end
+
+import Base: (==), size, getindex, setindex!, length, push!, empty!, isempty, eltype, getproperty, view
+mutable struct CircularArrayBuffer{A}
+    capacity::Int
+    first::Int
+    length::Int
+    stepsize::Int
+    buffer::A
+
+    CircularArrayBuffer{A}(capacity::Int, element_size::Vararg{Int, N}) where {A, N} = new{A}(capacity, 1, 0, *(element_size...), A(undef, element_size..., capacity))
+    CircularArrayBuffer{A}(capacity::Int, element_size::Tuple{Vararg{Int, N}}) where {A, N} = CircularArrayBuffer{A}(capacity, element_size...)
+end
+
+function CircularArrayBuffer(capacity::Int, element::AbstractArray{T, N}) where {T, N}
+    t = typeof(element)
+    CircularArrayBuffer{t.name.wrapper{t.parameters[1], N+1}}(capacity, size(element))
+end
+
+@inline function _buffer_index(cb::CircularArrayBuffer, i::Int)
+    n = cb.capacity
+    idx = cb.first + i - 1
+    if idx > n
+        idx - n
+    else
+        idx
+    end
+end
+@inline function _buffer_index(cb::CircularArrayBuffer, i::UnitRange{Int})
+    start = _buffer_index(cb, i.start)
+    stop = _buffer_index(cb, i.stop)
+    start ≤ stop ? (start:stop) : vcat(start:cb.capacity, 1:stop)
+end
+Base.@propagate_inbounds function _buffer_index_checked(cb::CircularArrayBuffer, i::Int)
+    @boundscheck if i < 1 || i > cb.length
+        throw(BoundsError(cb, i))
+    end
+    _buffer_index(cb, i)
+end
+
+for func in [:view, :getindex]
+    for N in 2:5
+         @eval @__MODULE__() begin
+             @inline Base.@propagate_inbounds $func(cb::CircularArrayBuffer{<:AbstractArray{T, $N}}, i::Int) where {T, N} = $func(cb.buffer, $(fill(Colon(), N-1)...),  _buffer_index_checked(cb, i))
+             $func(cb::CircularArrayBuffer{<:AbstractArray{T, $N}}, i::UnitRange{Int}) where {T, N} = $func(cb.buffer, $(fill(Colon(), N-1)...),  _buffer_index(cb, i))
+             $func(cb::CircularArrayBuffer{<:AbstractArray{T, $N}}, I::Vector{Int}) where {T, N} = $func(cb.buffer, $(fill(Colon(), N-1)...), [_buffer_index(cb, i) for i in I])
+             $(Symbol(func, :consecutive))(cb::CircularArrayBuffer{<:AbstractArray{T, $N}}, I::Vector{Int}, n::Int) where {T, N} = reshape($func(cb, [j for i in I for j in i-n+1:i]), $([:(size(cb.buffer, $i)) for i in 1:N-2]...), n * size(cb.buffer, $(N-1)), length(I))
+        end
+    end
+end
+
+for N in 1:4
+    @eval @__MODULE__() begin
+        function getindexconsecutive(cb::CircularBuffer{<:AbstractArray{T, $N}}, I::Vector{Int}, n) where {T}
+            elemsize = size(cb.buffer[1])
+            stepsize = *(elemsize...)
+            result = Array{Float64, $(N + 1)}(undef, $([:(elemsize[$i]) for i in 1:N-1]...), n * elemsize[$N], length(I))
+            start = 1
+            for i in I
+                for j in n:-1:1
+                    unsafe_copyto!(result, start, cb[i - j + 1], 1, stepsize)
+                    start += stepsize
+                end
+            end
+            result
+        end
+    end
+end
+
+"""
+    push!(cb::CircularArrayBuffer{T, N}, data::E) where {T, N}
+
+Add an element to the back and overwrite front if full.
+Make sure that `length(data) == cb.stepsize`
+"""
+@inline function push!(cb::CircularArrayBuffer{<:AbstractArray{T, N1}}, data::AbstractArray{T, N2}) where {T, N1, N2}
+    N1 == N2 + 1 || error("Cannot push data ot type $(typeof(data)) to buffer of type $(typeof(cb))")
+#     length(data) == cb.stepsize || throw(DimensionMismatch("the length of buffer's stepsize doesn't match the length of data, $(cb.stepsize) != $(length(data))"))
+    # if full, increment and overwrite, otherwise push
+    if cb.length == cb.capacity
+        cb.first = (cb.first == cb.capacity ? 1 : cb.first + 1)
+    else
+        cb.length += 1
+    end
+    nxt_idx = _buffer_index(cb, cb.length)
+    @inbounds cb.buffer[cb.stepsize * (nxt_idx - 1) + 1: cb.stepsize * nxt_idx] = data
+    cb
+end
+
+@inline function push!(cb::CircularArrayBuffer{<:AbstractArray{T, 1}}, data::T) where {T, N}
+    if cb.length == cb.capacity
+        cb.first = (cb.first == cb.capacity ? 1 : cb.first + 1)
+    else
+        cb.length += 1
+    end
+    @inbounds cb.buffer[_buffer_index(cb, cb.length)] = data
+    cb
+end
+
+length(cb::CircularArrayBuffer) = cb.length
+isfull(cb::CircularArrayBuffer) = length(cb) == capacity(cb)
+isempty(cb::CircularArrayBuffer) = length(cb) == 0
+empty!(cb::CircularArrayBuffer) = (cb.length = 0; cb)
+
+
 """
     ArrayCircularBuffer(arraytype, datatype, elemshape, capacity)
 """
