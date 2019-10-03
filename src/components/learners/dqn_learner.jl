13fe5a35ba0f19f810b20a4b33d9667b608a2305
Jun Tian <tianjun.cpp@gmail.com>
2019-10-04 01:26:06 +0800

    update dqn learners

--- src/components/learners/dqn_learner.jl
+++ src/components/learners/dqn_learner.jl
@@ -6,81 +6,49 @@ using StatsBase
 """
 https://www.nature.com/articles/nature14236
 """
-mutable struct DQNLearner{Tq<:AbstractQApproximator,Tf,Tl} <: AbstractLearner
+Base.@kwdef mutable struct DQNLearner{Tq<:AbstractQApproximator,Tf} <: AbstractLearner
     approximator::Tq
     target_approximator::Tq
     loss_fun::Tf
-    γ::Float32
-    batch_size::Int
-    update_horizon::Int
-    min_replay_history::Int
-    update_freq::Int
-    target_update_freq::Int
-    update_step::Int
-    loss::Tl
-    # ??? can the code bellow simplified?
-    function DQNLearner(
-        ;
-        approximator::Tq,
-        target_approximator::Tq,
-        loss_fun::Tf,
-        γ::Float32 = 0.99f0,
-        batch_size::Int = 32,
-        update_horizon::Int = 1,
-        min_replay_history::Int = 32,
-        update_freq::Int = 1,
-        target_update_freq::Int = 100,
-        update_step::Int = 0,
-        loss::Tl = 0.f0,
-    ) where {Tq,Tf,Tl}
+    γ::Float32 = 0.99f0
+    batch_size::Int = 32
+    update_horizon::Int = 1
+    min_replay_history::Int = 32
+    update_freq::Int = 1
+    target_update_freq::Int = 100
+    update_step::Int = 0
+    loss::Float32 = 0.f0
 
-        copyto!(approximator, target_approximator)  # force sync
-        new{Tq,Tf,Tl}(
-            approximator,
-            target_approximator,
-            loss_fun,
-            γ,
-            batch_size,
-            update_horizon,
-            min_replay_history,
-            update_freq,
-            target_update_freq,
-            update_step,
-            loss,
-        )
+    function DQNLearner(approximator::Tq, target_approximator::Tq, loss_fun::Tf, args...) where {Tq, Tf}
+        copyto!(approximator, target_approximator)
+        new{Tq, Tf}(approximator, target_approximator, loss_fun, args...)
     end
-end
-
-function DQNLearner(Q, target_Q, loss_fun; kw...)
-    copyto!(target_Q, Q)
-    DQNLearner(
-        ;
-        approximator = Q,
-        target_approximator = target_Q,
-        loss_fun = loss_fun,
-        kw...,
-    )
 end
 
 function update!(learner::DQNLearner{<:NeuralNetworkQ}, batch)
     learner.update_step += 1
     learner.update_step % learner.update_freq == 0 || return nothing
 
-    Q, Qₜ, γ, loss_fun, update_horizon = learner.approximator,
+    Q, Qₜ, γ, loss_fun, update_horizon, batch_size = learner.approximator,
         learner.target_approximator,
         learner.γ,
         learner.loss_fun,
-        learner.update_horizon
-    states, actions, rewards, terminals, next_states = batch
+        learner.update_horizon,
+        learner.batch_size
+    states, rewards, terminals, next_states = map(x->to_device(Q, x), (batch.states, batch.rewards, batch.terminals, batch.next_states))
+    actions = CartesianIndex.(batch.actions, 1:batch_size) 
 
-    q = batch_estimate(Q, states, actions)
-    q′ = dropdims(maximum(Qₜ(next_states); dims = 1), dims = 1)
-    G = rewards .+ γ^update_horizon .* (1 .- terminals) .* q′
+    loss, back = Flux.pullback(Q.params) do 
+        q = batch_estimate(Q, states)[actions]
+        q′ = dropdims(maximum(batch_estimate(Qₜ, next_states); dims = 1), dims = 1)
+        G = rewards .+ γ^update_horizon .* (1 .- terminals) .* q′
 
-    batch_losses = loss_fun(G, q)
-    loss = mean(batch_losses)
-    learner.loss = loss.data
-    update!(Q, loss)
+        batch_losses = loss_fun(G, q)
+        mean(batch_losses)
+    end
+
+    learner.loss = loss
+    update!(Q, back(loss))
 
     if learner.update_step % learner.target_update_freq == 0
         copyto!(Qₜ, Q)
--- src/components/learners/rainbow.jl
+++ src/components/learners/rainbow.jl
@@ -1,70 +1,32 @@
 export RainbowLearner, update!
 
-using Flux
+using Flux, Zygote
 using StatsBase
 
-mutable struct RainbowLearner{Tq<:AbstractQApproximator,Tf,Tl,Ts} <: AbstractLearner
+Base.@kwdef mutable struct RainbowLearner{Tq<:AbstractQApproximator,Tf,Ts} <: AbstractLearner
     approximator::Tq
     target_approximator::Tq
     loss_fun::Tf
-    γ::Float32
-    loss::Tl
-    batch_size::Int
-    update_horizon::Int
-    min_replay_history::Int
-    update_freq::Int
-    target_update_freq::Int
-    update_step::Int
     Vₘₐₓ::Float32
     Vₘᵢₙ::Float32
-    delta_z::Float32
-    n_atoms::Int
     n_actions::Int
-    support::Ts
-    default_priority::Float64
-end
+    n_atoms::Int = 51
+    support::Ts = collect(range(Float32(-Vₘₐₓ), Float32(Vₘₐₓ), length = n_atoms))
+    delta_z::Float32 = Float32(support[2] - support[1])
+    γ::Float32 = 0.99
+    loss::Float32 = 0.f0
+    batch_size::Int = 32
+    update_horizon::Int = 1
+    min_replay_history::Int = 32
+    update_freq::Int = 1
+    target_update_freq::Int = 500
+    update_step::Int = 0
+    default_priority::Float64 = 100.0
 
-function RainbowLearner(
-    ;
-    approximator,
-    target_approximator,
-    loss_fun,
-    Vₘₐₓ,
-    Vₘᵢₙ,
-    n_actions,
-    γ = 0.99,
-    loss = 0.f0,
-    update_freq = 1,
-    target_update_freq = 500,
-    update_step = 0,
-    n_atoms = 51,
-    batch_size = 32,
-    update_horizon = 1,
-    min_replay_history = 32,
-    default_priority = 100.0,
-)
-    support = range(Float32(-Vₘₐₓ), Float32(Vₘₐₓ), length = n_atoms)
-    copyto!(approximator, target_approximator)  # force sync
-    RainbowLearner(
-        approximator,
-        target_approximator,
-        loss_fun,
-        γ,
-        loss,
-        batch_size,
-        update_horizon,
-        min_replay_history,
-        update_freq,
-        target_update_freq,
-        update_step,
-        Vₘₐₓ,
-        Vₘᵢₙ,
-        Float32(support.step),
-        n_atoms,
-        n_actions,
-        collect(support),
-        default_priority,
-    )
+    function RainbowLearner(approximator::Tq, target_approximator::Tq, loss_fun::Tf, Vₘₐₓ::Float32, Vₘᵢₙ::Float32, n_actions::Int, n_atoms::Int, support::Ts, args...) where {Tq,Tf, Ts}
+        copyto!(approximator, target_approximator)  # force sync
+        new{Tq,Tf, Ts}(approximator, target_approximator, loss_fun, Vₘₐₓ, Vₘᵢₙ, n_actions, n_atoms, support, args...)
+    end
 end
 
 function (learner::RainbowLearner)(obs::Observation)
@@ -78,7 +40,7 @@ function update!(learner::RainbowLearner, batch)
     learner.update_step += 1
     learner.update_step % learner.update_freq == 0 || return nothing
 
-    Q, Qₜ, γ, loss_fun, n_atoms, n_actions, support, delta_z, update_horizon = learner.approximator,
+    Q, Qₜ, γ, loss_fun, n_atoms, n_actions, support, delta_z, update_horizon, batch_size = learner.approximator,
         learner.target_approximator,
         learner.γ,
         learner.loss_fun,
@@ -86,41 +48,48 @@ function update!(learner::RainbowLearner, batch)
         learner.n_actions,
         learner.support,
         learner.delta_z,
-        learner.update_horizon
+        learner.update_horizon,
+        learner.batch_size
 
-    states, actions, rewards, terminals, next_states = batch
+    states, rewards, terminals, next_states = map(x->to_device(Q, x), (batch.states, batch.rewards, batch.terminals, batch.next_states))
+    actions = CartesianIndex.(batch.actions, 1:batch_size)
 
     target_support = reshape(rewards, 1, :) .+
                      (reshape(support, :, 1) *
                       reshape((γ^update_horizon) .* (1 .- terminals), 1, :))
 
-    logits = reshape(Q(states), n_atoms, n_actions, :)
-    select_logits = logits[:, [CartesianIndex(a, i) for (i, a) in enumerate(actions)]]
-    next_logits = Qₜ(next_states).data
-    next_probs = reshape(softmax(reshape(next_logits, n_atoms, :)), n_atoms, n_actions, :)
-    next_q = reshape(sum(support .* next_probs, dims = 1), n_actions, :)
-    # next_q_argmax = argmax(cpu(next_q .+ next_legal_actions), dims=1)
-    next_q_argmax = argmax(next_q, dims = 1)
-    next_prob_select = reshape(next_probs[:, next_q_argmax], n_atoms, :)
+    updated_priorities = Vector{Float32}()
 
-    target_distribution = project_distribution(
-        target_support,
-        next_prob_select,
-        support,
-        delta_z,
-        learner.Vₘᵢₙ,
-        learner.Vₘₐₓ,
-    )
+    loss, back = Flux.pullback(Q.params) do 
+        logits = reshape(batch_estimate(Q, states), n_atoms, n_actions, :)
+        select_logits = logits[:, actions]
+        next_logits = batch_estimate(Qₜ, next_states)
+        next_probs = reshape(softmax(reshape(next_logits, n_atoms, :)), n_atoms, n_actions, :)
+        next_q = reshape(sum(support .* next_probs, dims = 1), n_actions, :)
+        # next_q_argmax = argmax(cpu(next_q .+ next_legal_actions), dims=1)
+        next_q_argmax = argmax(next_q, dims = 1)
+        next_prob_select = reshape(next_probs[:, next_q_argmax], n_atoms, :)
 
-    batch_losses = loss_fun(select_logits, target_distribution)
-    updated_priorities = vec(clamp.(sqrt.(batch_losses.data .+ 1f-10), 1.f0, 1.f2))
+        target_distribution = project_distribution(
+            target_support,
+            next_prob_select,
+            support,
+            delta_z,
+            learner.Vₘᵢₙ,
+            learner.Vₘₐₓ,
+        )
 
-    target_priorities = 1.0f0 ./ sqrt.(updated_priorities .+ 1f-10)
-    target_priorities ./= maximum(target_priorities)
-    weighted_loss = mean(target_priorities .* batch_losses)
+        batch_losses = loss_fun(select_logits, target_distribution)
+        updated_priorities = vec(clamp.(sqrt.(Zygote.dropgrad(batch_losses) .+ 1f-10), 1.f0, 1.f2))
 
-    update!(Q, weighted_loss)
-    learner.loss = weighted_loss.data
+        target_priorities = 1.0f0 ./ sqrt.(updated_priorities .+ 1f-10)
+        target_priorities ./= maximum(target_priorities)
+
+        mean(Zygote.dropgrad(target_priorities) .* batch_losses)
+    end
+
+    learner.loss = loss
+    update!(Q, back(loss))
 
     if learner.update_step % learner.target_update_freq == 0
         copyto!(Qₜ, Q)
