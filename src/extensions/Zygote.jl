e4d64e2e28d05d361d460697047dc7059343fdc3
Jun Tian <tianjun.cpp@gmail.com>
2019-12-07 20:21:05 +0800

    rainbow is still slow, not sure why

--- src/extensions/Zygote.jl
+++ src/extensions/Zygote.jl
@@ -6,19 +6,3 @@ import Zygote:gradient
 gradient(f, ::Val{:Zygote}, args...) = gradient(f, args...)
 
 Zygote.@adjoint argmax(xs; dims = :) = argmax(xs;dims=dims), _ -> nothing
-
-# ??? can safely removed now
-Zygote.@adjoint function Base.broadcasted(::typeof(relu), x::Array{T}) where T<:Real
-    y = relu.(x)
-    return y, Δ -> begin
-        res = similar(Δ)
-        for i in 1:length(res)
-            if y[i] > 0
-                res[i] = Δ[i]
-            else
-                res[i] = zero(T)
-            end
-        end
-        (nothing, res)
-    end
-end
