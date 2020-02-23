8e965250bc3b597f2c2d6235928b9726a3f67939
Jun Tian <tianjun.cpp@gmail.com>
2020-02-23 19:45:16 +0800

    clean dependency

--- src/ReinforcementLearningZoo/test/runtests.jl
+++ src/ReinforcementLearningZoo/test/runtests.jl
@@ -3,12 +3,7 @@ using Test
 using ReinforcementLearningBase
 using ReinforcementLearningCore
 using Flux
-using CUDAapi
 using StatsBase
-
-if has_cuda()
-    using CuArrays
-end
 
 @testset "ReinforcementLearningZoo.jl" begin
 
