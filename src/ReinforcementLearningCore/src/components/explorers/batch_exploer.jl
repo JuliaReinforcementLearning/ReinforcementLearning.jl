1a4aeb096d9b2dd1d08949d94f8903c8c99a6ae6
Jun Tian <find_my_way@foxmail.com>
2020-03-12 12:44:40 +0800

    extend BatchExplorer (#37)

--- src/ReinforcementLearningCore/src/components/explorers/batch_exploer.jl
+++ src/ReinforcementLearningCore/src/components/explorers/batch_exploer.jl
@@ -1,7 +1,14 @@
 export BatchExplorer
 
-struct BatchExplorer{E<:AbstractExplorer} <: AbstractExplorer
+"""
+    BatchExplorer(explorer::AbstractExplorer)
+    BatchExplorer(explorers::AbstractExplorer...)
+"""
+struct BatchExplorer{E} <: AbstractExplorer
     explorer::E
 end
 
+BatchExplorer(explorers::AbstractExplorer...) = BatchExplorer(explorers)
+
 (x::BatchExplorer)(values::AbstractMatrix) = [x.explorer(v) for v in eachcol(values)]
+(x::BatchExplorer{<:Tuple})(values::AbstractMatrix) = [explorer(v) for (explorer, v) in zip(x.explorer, eachcol(values))]
