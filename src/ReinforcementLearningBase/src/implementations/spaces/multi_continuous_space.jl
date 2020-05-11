9574ae0fad5f256d43ae68fd6e9e6d9a1aa3b2b6
jbrea <jbrea@users.noreply.github.com>
2020-05-11 09:21:51 +0200

    Update src/implementations/spaces/multi_continuous_space.jl
    Co-authored-by: Jun Tian <find_my_way@foxmail.com>

--- src/ReinforcementLearningBase/src/implementations/spaces/multi_continuous_space.jl
+++ src/ReinforcementLearningBase/src/implementations/spaces/multi_continuous_space.jl
@@ -21,7 +21,7 @@ Similar to [`ContinuousSpace`](@ref), but scaled to multi-dimension.
 """
 MultiContinuousSpace(low, high) = MultiContinuousSpace(promote(low, high)...)
 
-Base.eltype(::MultiContinuousSpace{T}) where {T} = eltype(T)
+Base.eltype(::MultiContinuousSpace{T}) where {T} = T
 Base.in(xs, s::MultiContinuousSpace) =
     size(xs) == size(s.low) && all(map((l, x, h) -> l <= x <= h, s.low, xs, s.high))
 
