d301ba6ae8a65c0936e3215c39b44ff5a4da1c27
Jun Tian <find_my_way@foxmail.com>
2020-05-13 23:24:24 +0800

    remove the default implementation (#68)

--- src/ReinforcementLearningCore/src/components/learners/learners.jl
+++ src/ReinforcementLearningCore/src/components/learners/learners.jl
@@ -12,18 +12,6 @@ abstract type AbstractLearner end
 function (learner::AbstractLearner)(obs) end
 
 """
-    update!(learner::AbstractLearner, experience)
-
-Typical `experience` is [`AbstractTrajectory`](@ref).
-"""
-function RLBase.update!(learner::AbstractLearner, t::AbstractTrajectory)
-    experience = extract_experience(t, learner)
-    isnothing(experience) || update!(learner, experience)
-end
-
-function extract_experience end
-
-"""
     get_priority(p::AbstractLearner, experience)
 """
 function RLBase.get_priority(p::AbstractLearner, experience) end
