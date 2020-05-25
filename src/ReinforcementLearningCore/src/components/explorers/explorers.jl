d26c8299dec07612ae2c2cfb0dd8de64d356f4c2
Felix Chalumeau <49362657+felixchalumeau@users.noreply.github.com>
2020-05-25 10:23:19 +0200

    Quick name correction in a batch_explorer file (#72)
    * Rename batch_expoer to batch_explorer
    
    * update import name of batch_explorer.jl

--- src/ReinforcementLearningCore/src/components/explorers/explorers.jl
+++ src/ReinforcementLearningCore/src/components/explorers/explorers.jl
@@ -1,5 +1,5 @@
 include("abstract_explorer.jl")
-include("batch_exploer.jl")
+include("batch_explorer.jl")
 include("epsilon_greedy_explorer.jl")
 include("UCB_explorer.jl")
 include("weighted_explorer.jl")
