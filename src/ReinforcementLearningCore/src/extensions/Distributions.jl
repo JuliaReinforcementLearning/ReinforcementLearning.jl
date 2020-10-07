8b59a8d2324382132ba52ca89f954b3656b0dbaa
Jun Tian <find_my_way@foxmail.com>
2020-10-07 17:13:39 +0800

    Update Distributions.jl

--- src/ReinforcementLearningCore/src/extensions/Distributions.jl
+++ src/ReinforcementLearningCore/src/extensions/Distributions.jl
@@ -13,8 +13,3 @@ function normlogpdf(μ, σ, x; ϵ = 1.0f-8)
     z = (x .- μ) ./ (σ .+ ϵ)
     -(z .^ 2 .+ log(2.0f0π)) / 2.0f0 .- log.(σ .+ ϵ)
 end
-
-
-# watch https://github.com/JuliaStats/Distributions.jl/pull/1184
-
-Base.convert(::Type{DiscreteNonParametric{T,P,Ts,Ps}}, d::DiscreteNonParametric) where {T,P,Ts,Ps} = DiscreteNonParametric{T,P,Ts,Ps}(convert(Ts, support(d)), convert(Ps, probs(d)), check_args=false)
