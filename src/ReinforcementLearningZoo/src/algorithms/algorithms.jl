6c12b5e5a7aaefe4ef78549303245beceef4218f
norci <norci@users.noreply.github.com>
2020-11-30 09:01:39 +0800

    updated patch.jl, for ignore function. (#127)

--- src/ReinforcementLearningZoo/src/algorithms/algorithms.jl
+++ src/ReinforcementLearningZoo/src/algorithms/algorithms.jl
@@ -1,3 +1,5 @@
+using Zygote: ignore
+
 include("dqns/dqns.jl")
 include("policy_gradient/policy_gradient.jl")
 include("searching/searching.jl")
--- src/ReinforcementLearningZoo/src/patch.jl
+++ src/ReinforcementLearningZoo/src/patch.jl
@@ -1,10 +1,3 @@
-# watch https://github.com/FluxML/Zygote.jl/pull/623
-
-using Zygote
-
-ignore(f) = f()
-Zygote.@adjoint ignore(f) = ignore(f), _ -> nothing
-
 using ReinforcementLearningCore
 
 (app::NeuralNetworkApproximator)(args...; kwargs...) = app.model(args...; kwargs...)
