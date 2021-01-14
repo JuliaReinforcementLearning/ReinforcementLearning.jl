0f518b7fd8066de2d7afe8b2a787803ca4ea4516
Sriram <sriram13k@gmail.com>
2021-01-14 17:27:52 +0530

    Refactor exports (#122)
    * Refactor exports
    
    Remove GoalState as it is supported by RLBase
    
    * Update compat of RLBase
    
    Bump version of RLBase

--- src/ReinforcementLearningEnvironments/src/environments/examples/BitFlippingEnv.jl
+++ src/ReinforcementLearningEnvironments/src/environments/examples/BitFlippingEnv.jl
@@ -1,13 +1,10 @@
-export BitFlippingEnv, GoalState
+export BitFlippingEnv
 
 """
 In Bit Flipping Environment we have n bits. The actions are 1 to n where executing i-th action flips the i-th bit of the state. For every episode we sample uniformly and inital state as well as the target state.
 
 Refer [Hindsight Experience Replay paper](https://arxiv.org/pdf/1707.01495.pdf) for the motivation behind the environment.
 """
-
-struct GoalState{T} <: RLBase.AbstractStateStyle end
-GoalState() = GoalState{Any}()
 
 struct BitFlippingEnv <: AbstractEnv
     N::Int
