17090f348e7e3f592cfb28dfaddccf55a9b81c7c
Jun Tian <tianjun.cpp@gmail.com>
2021-04-27 22:42:14 +0800

    setup local dev environment

--- src/ReinforcementLearning.jl
+++ src/ReinforcementLearning.jl
@@ -5,14 +5,9 @@ const RL = ReinforcementLearning
 
 using Reexport
 
-include("ReinforcementLearningBase/src/ReinforcementLearningBase.jl")
-include("ReinforcementLearningCore/src/ReinforcementLearningCore.jl")
-include("ReinforcementLearningEnvironments/src/ReinforcementLearningEnvironments.jl")
-include("ReinforcementLearningZoo/src/ReinforcementLearningZoo.jl")
-
-@reexport using .ReinforcementLearningBase
-@reexport using .ReinforcementLearningCore
-@reexport using .ReinforcementLearningEnvironments
-@reexport using .ReinforcementLearningZoo
+@reexport using ReinforcementLearningBase
+@reexport using ReinforcementLearningCore
+@reexport using ReinforcementLearningEnvironments
+@reexport using ReinforcementLearningZoo
 
 end
