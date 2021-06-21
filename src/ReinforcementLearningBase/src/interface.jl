7671d8a0dea4988d24aa3d7e5da00c0ef609385b
Jun Tian <find_my_way@foxmail.com>
2021-06-21 11:38:36 +0800

    Add a warning in docstring of state (#327)
    Address the concern in https://github.com/JuliaReinforcementLearning/GridWorlds.jl/pull/146#issuecomment-864541170

--- src/ReinforcementLearningBase/src/interface.jl
+++ src/ReinforcementLearningBase/src/interface.jl
@@ -448,6 +448,10 @@ The state can be of any type. However, most neural network based algorithms
 assume an `AbstractArray` is returned. For environments with many different states
 provided (inner state, information state, etc), users need to provide `style`
 to declare which kind of state they want.
+            
+!!! warning
+    The state **may** be reused and be mutated at each step. Always remember to make a copy
+    if this is not what you expect.
 """
 @multi_agent_env_api state(env::AbstractEnv) = state(env, DefaultStateStyle(env))
 state(env::AbstractEnv, ss::AbstractStateStyle) = state(env, ss, current_player(env))
