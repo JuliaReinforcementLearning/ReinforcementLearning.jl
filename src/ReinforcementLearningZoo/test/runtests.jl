04f8e618ecc83244d003006311e7983a9b69ad9a
peter <51195500031@stu.ecnu.edu.cn>
2021-07-07 16:18:07 +0800

    add get_optimal_kuhn_policy for KuhnPokerEnv

--- src/ReinforcementLearningZoo/test/runtests.jl
+++ src/ReinforcementLearningZoo/test/runtests.jl
@@ -9,23 +9,43 @@ using Random
 using StableRNGs
 using OpenSpiel
 
-function get_optimal_kuhn_policy(α = 0.2)
-    TabularRandomPolicy(
-        table = Dict(
-            "0" => [1 - α, α],
-            "0pb" => [1.0, 0.0],
-            "1" => [1.0, 0.0],
-            "1pb" => [2.0 / 3.0 - α, 1.0 / 3.0 + α],
-            "2" => [1 - 3 * α, 3 * α],
-            "2pb" => [0.0, 1.0],
-            "0p" => [2.0 / 3.0, 1.0 / 3.0],
-            "0b" => [1.0, 0.0],
-            "1p" => [1.0, 0.0],
-            "1b" => [2.0 / 3.0, 1.0 / 3.0],
-            "2p" => [0.0, 1.0],
-            "2b" => [0.0, 1.0],
-        ),
-    )
+function get_optimal_kuhn_policy(env, α = 0.2)
+    if typeof(env) == KuhnPokerEnv
+        TabularRandomPolicy(
+            table = Dict(
+                (:J,) => [1 - α, α],
+                (:J, :pass, :bet) => [1.0, 0.0],
+                (:Q,) => [1.0, 0.0],
+                (:Q, :pass, :bet) => [2.0 / 3.0 - α, 1.0 / 3.0 + α],
+                (:K,) => [1 - 3 * α, 3 * α],
+                (:K, :pass, :bet) => [0.0, 1.0],
+                (:J, :pass) => [2.0 / 3.0, 1.0 / 3.0],
+                (:J, :bet) => [1.0, 0.0],
+                (:Q, :pass) => [1.0, 0.0],
+                (:Q, :bet) => [2.0 / 3.0, 1.0 / 3.0],
+                (:K, :pass) => [0.0, 1.0],
+                (:K, :bet) => [0.0, 1.0],
+            ),
+        )
+
+    else
+        TabularRandomPolicy(
+            table = Dict(
+                "0" => [1 - α, α],
+                "0pb" => [1.0, 0.0],
+                "1" => [1.0, 0.0],
+                "1pb" => [2.0 / 3.0 - α, 1.0 / 3.0 + α],
+                "2" => [1 - 3 * α, 3 * α],
+                "2pb" => [0.0, 1.0],
+                "0p" => [2.0 / 3.0, 1.0 / 3.0],
+                "0b" => [1.0, 0.0],
+                "1p" => [1.0, 0.0],
+                "1b" => [2.0 / 3.0, 1.0 / 3.0],
+                "2p" => [0.0, 1.0],
+                "2b" => [0.0, 1.0],
+            ),
+        )
+    end
 end
 
 @testset "ReinforcementLearningZoo.jl" begin
