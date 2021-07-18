eda815cf7fc8a5c1d274de19ee9cae11db722169
peter <csherry1231@gmail.com>
2021-07-18 13:56:53 +0800

    add weighted_softmax_explorer in the explorers

--- src/ReinforcementLearningCore/src/policies/q_based_policies/explorers/explorers.jl
+++ src/ReinforcementLearningCore/src/policies/q_based_policies/explorers/explorers.jl
@@ -3,6 +3,7 @@ include("batch_explorer.jl")
 include("epsilon_greedy_explorer.jl")
 include("UCB_explorer.jl")
 include("weighted_explorer.jl")
+include("weighted_softmax_explorer.jl")
 include("gumbel_softmax_explorer.jl")
 # TODO 
 # https://arxiv.org/abs/1509.02971
