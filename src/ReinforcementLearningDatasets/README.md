efc4e6cc6cb88ae232d109fb27c30ff56c530f49
Mobius1D <prsdhsk@gmail.com>
2021-07-19 11:22:37 +0530

    Documentation changes
    Documentation and some other code changes

--- src/ReinforcementLearningDatasets/README.md
+++ src/ReinforcementLearningDatasets/README.md
@@ -1,1 +1,7 @@
 # ReinforcementLearningDatasets
+
+A package to create, manage, store and retrieve datasets for Offline Reinforcement Learning using ReinforcementLearning.jl package.
+
+### Note:
+
+The package is under active development and for now it supports only a limited number of 3rd party datasets.
--- src/ReinforcementLearningDatasets/src/3rd_party/3rd_party_datasets.jl
+++ src/ReinforcementLearningDatasets/src/3rd_party/3rd_party_datasets.jl
@@ -6,10 +6,24 @@ using ReinforcementLearning: SART, SARTS, BatchSampler
 
 export get_dataset, env_names, install_packages
 
+"""
+    get_dataset(env_name, package)
+
+This function fetches the dataset for the gym `env_name` from the `package` that is specified. This is intended to be a 3rd party wrapper that would return a NamedTuple.
+The type of the NamedTuple depends on the dataset that is requested. For now d4rl package envs would return a NamedTuple{SARTS} and the rest would return a NamedTuple{SART}.
+
+- `env_name` is the name of the gym env that the dataset is provided for.
+- `package` is the name of the offline learning dataset package. For now supports `d4rl`, `d4rl_pybullet` and `d4rl_atari` for now.
+
+`package` defaults to "d4rl" unless specified.
+
+WARNING: It is advisable to use `atari` package envs with a sufficient amount of RAM because it rapidly fills up a huge amount of memory.
+"""
+
 # supports d4rl, pybullet and atari offline datasets
 # TO-DO: get more info on mujoco envs
-# TO-DO: give warning for getting atari datasets
-function get_dataset(env_name::String, package::String)
+# TO-DO: give warning for loading atari datasets
+function get_dataset(env_name::String, package::String = "d4rl")
     
     data = get_data(env_name, package)
         
@@ -25,7 +39,6 @@ function get_dataset(env_name::String, package::String)
                     reward=copy(data["rewards"]'),
                     terminal=copy(data["terminals"]'),)
         return dataset
-
     else 
         error(
         "The package specified is not supported or does not exist"
@@ -34,6 +47,7 @@ function get_dataset(env_name::String, package::String)
 
 end
 
+# Intended as a helper function that would fetch the data for the env and package that is specified before packaging it into a NamedTuple.
 function get_data(env_name::String, package::String) 
     
     if !PyCall.pyexists("gym")
@@ -60,7 +74,9 @@ function get_data(env_name::String, package::String)
     if package == "d4rl_atari"
         run(`$(PyCall.python) -m atari_py.import_roms $(ROM_PATH)`);
     end
+    
     pyimport(package)
+    
     if ! (env_name in env_names(package))
         error(
             "Dataset for this environment doesn't exist in package"
@@ -73,7 +89,18 @@ function get_data(env_name::String, package::String)
     
 end
 
+"""
+    env_names(package)
+
+Fetches the list of `env`s that are available for the specified `package`.
+Supported packages are:
+
+- `d4rl`
+- `d4rl_pybullet`
+- `d4rl_atari`
+"""
 function env_names(package::String)
+    
     if !PyCall.pyexists("gym")
         error(
             "Cannot import module 'gym'.\n\nIf you did not yet install it, try running\n`ReinforcementLearningEnvironments.install_gym()`\n",
@@ -83,11 +110,13 @@ function env_names(package::String)
     gym =  pyimport("gym")
     
     pyimport(package)
+    
     modules = [
         "d4rl_pybullet.envs"
         "d4rl.gym_mujoco.gym_envs"
         "d4rl_atari.envs"
     ]
+    
     if package == "d4rl_atari"
         run(`$(PyCall.python) -m atari_py.import_roms $(ROM_PATH)`);
     end
@@ -97,6 +126,11 @@ end
 
 # Check if there are other dependencies on the package
 # Make sure that you install mujoco and mujocopy before installing the below packages
+"""
+    install_packages(packages)
+
+Installs the specified packages using pip. Still under development and not tested on other configurations.
+"""
 function install_packages(packages = ["d4rl_pybullet", "d4rl_atari", "d4rl"])
     proxy_arg = String[]
     if haskey(ENV, "http_proxy")
