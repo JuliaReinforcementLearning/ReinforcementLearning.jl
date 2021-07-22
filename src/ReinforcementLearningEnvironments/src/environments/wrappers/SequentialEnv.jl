b71fb704479cd5a29df884b79116ec6b7614c3f4
Jun Tian <tianjun.cpp@gmail.com>
2021-07-22 14:24:00 +0800

    add more meaningful tests

--- src/ReinforcementLearningEnvironments/src/environments/wrappers/SequentialEnv.jl
+++ src/ReinforcementLearningEnvironments/src/environments/wrappers/SequentialEnv.jl
@@ -19,8 +19,20 @@ RLBase.DynamicStyle(env::SequentialEnv) = SEQUENTIAL
 
 RLBase.current_player(env::SequentialEnv) = env.current_player_idx
 
+RLBase.legal_action_space(env::SequentialEnv) = legal_action_space(env, current_player(env))
+RLBase.legal_action_space(env::SequentialEnv, player) = legal_action_space(env.env, player)
+
 RLBase.action_space(env::SequentialEnv) = action_space(env, current_player(env))
 RLBase.action_space(env::SequentialEnv, player) = action_space(env.env, player)
+
+function RLBase.reset!(env::SequentialEnv)
+    env.current_player_idx = 1
+    reset!(env.env)
+end
+
+RLBase.reward(env::SequentialEnv) = reward(env, current_player(env))
+
+RLBase.reward(env::SequentialEnv, player) = current_player(env) == 1 ? reward(env.env, player) : 0
 
 function (env::SequentialEnv)(action)
     env.actions[env.current_player_idx] = action
