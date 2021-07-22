a46cfff9483a1ccb2b9166f1ce4e600c69563f93
Jun Tian <find_my_way@foxmail.com>
2021-07-22 14:54:48 +0800

    AddSequentialEnv (#394)
    * add SequentialEnv wrapper
    
    * add more meaningful tests

--- src/ReinforcementLearningEnvironments/test/environments/wrappers/wrappers.jl
+++ src/ReinforcementLearningEnvironments/test/environments/wrappers/wrappers.jl
@@ -103,4 +103,45 @@
         RLBase.test_runnable!(env′)
     end
 
+    @testset "SequentialEnv" begin
+        env =  RockPaperScissorsEnv()
+        env′ = SequentialEnv(env)
+        RLBase.test_interfaces!(env′)
+        RLBase.test_runnable!(env′)
+
+        # https://github.com/JuliaReinforcementLearning/ReinforcementLearning.jl/issues/393
+        @test RLBase.current_player(env′) == 1
+        @test action_space(env′) == action_space(env′, 1)
+
+        env′('💎')
+
+        @test RLBase.current_player(env′) == 2
+        @test is_terminated(env′) == false
+
+        reset!(env′)
+
+        @test RLBase.current_player(env′) == 1
+        @test reward(env′) == 0
+        @test is_terminated(env′) == false
+
+        env′('💎')
+
+        @test reward(env′) == 0
+        @test reward(env′, 1) == 0
+        @test reward(env′, 2) == 0
+        @test is_terminated(env′) == false
+
+        env′('📃')
+
+        @test reward(env′, 1) == -1
+        @test reward(env′, 2) == 1
+        @test is_terminated(env′) == true
+
+        reset!(env′)
+        @test reward(env′) == 0
+        @test reward(env′, 1) == 0
+        @test reward(env′, 2) == 0
+        @test is_terminated(env′) == false
+        @test RLBase.current_player(env′) == 1
+    end
 end
