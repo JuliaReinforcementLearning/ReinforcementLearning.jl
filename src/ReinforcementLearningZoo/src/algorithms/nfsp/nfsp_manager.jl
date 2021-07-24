d6421e6d1a88d7734160f9fa7c333dbfa536afce
peter <csherry1231@gmail.com>
2021-07-25 07:04:21 +0800

    add NFSPManager

---
+++ src/ReinforcementLearningZoo/src/algorithms/nfsp/nfsp_manager.jl
@@ -0,0 +1,42 @@
+export NFSPAgentManager
+
+
+"""
+    NFSPAgentManager(; agents::Dict{Any, NFSPAgent})
+
+A special MultiAgentManager in which all agents use NFSP policy to play the game.
+"""
+mutable struct NFSPAgentManager <: AbstractPolicy
+    agents::Dict{Any, NFSPAgent}
+end
+
+function NFSPAgentManager(env::AbstractEnv; kwargs...)
+    NFSPAgentManager(
+        Dict((player, NFSPAgent(env, player; kwargs...)) 
+        for player in players(env) if player != chance_player(env)
+        )
+    )
+end
+
+function (π::NFSPAgentManager)(env::AbstractEnv)
+    player = current_player(env)
+    if player == chance_player(env)
+        env |> legal_action_space |> rand |> env
+    else
+        env |> π.agents[player] |> env
+    end
+end
+
+function RLBase.prob(π::NFSPAgentManager, env::AbstractEnv, args...)
+    agent = π.agents[current_player(env)].sl_agent
+    prob(agent.policy, env, args...)
+end
+
+function RLBase.update!(π::NFSPAgentManager, env::AbstractEnv)
+    player = current_player(env)
+    if player == chance_player(env)
+        env |> legal_action_space |> rand |> env
+    else
+        RLBase.update!(π.agents[player], env)
+    end
+end
