3ed3a2fe7d32c5f5b9bd249445484e3dbc6f296b
peter <csherry1231@gmail.com>
2021-07-26 03:45:29 +0800

    add NFSPAgent setting details in  NFSP_KuhnPoker experiment file

--- docs/experiments/experiments/NFSP/JuliaRL_NFSP_KuhnPoker.jl
+++ docs/experiments/experiments/NFSP/JuliaRL_NFSP_KuhnPoker.jl
@@ -2,7 +2,7 @@
 # title: JuliaRL\_NFSP\_KuhnPoker 
 # cover: assets/logo.svg 
 # description: NFSP applied to KuhnPokerEnv 
-# date: 2021-07-25
+# date: 2021-07-26
 # author: "[Peter Chen](https://github.com/peterchen96)" 
 # --- 
 
@@ -35,6 +35,8 @@ function RL.Experiment(
     seed = 123,
 )
 
+    rng = StableRNG(seed)
+
     # Encode the KuhnPokerEnv's states for training.
     env = KuhnPokerEnv()
     wrapped_env = StateTransformedEnv(
@@ -42,32 +44,87 @@ function RL.Experiment(
         state_mapping = s -> [findfirst(==(s), state_space(env)) / length(state_space(env))], # for normalization
         state_space_mapping = ss -> [[findfirst(==(s), state_space(env)) / length(state_space(env))] for s in state_space(env)]
         )
+    
+    player = 1 # or 2
+    ns, na = length(state(wrapped_env, player)), length(action_space(wrapped_env, player))
 
-    # set parameters for NFSPAgentManager
-    nfsp = NFSPAgentManager(wrapped_env;
-            η = 0.1,
-            _device = Flux.cpu,
-            Optimizer = Flux.Descent,
-            rng = StableRNG(seed),
+    # construct rl_agent(use `DQN`) and sl_agent(use `BehaviorCloningPolicy`)
+    rl_agent = Agent(
+        policy = QBasedPolicy(
+            learner = DQNLearner(
+                approximator = NeuralNetworkApproximator(
+                    model = Chain(
+                        Dense(ns, 128, relu; init = glorot_normal(rng)),
+                        Dense(128, na; init = glorot_normal(rng))
+                    ) |> cpu,
+                    optimizer = Descent(0.01),
+                ),
+                target_approximator = NeuralNetworkApproximator(
+                    model = Chain(
+                        Dense(ns, 128, relu; init = glorot_normal(rng)),
+                        Dense(128, na; init = glorot_normal(rng))
+                    ) |> cpu,
+                ),
+                γ = 1.0f0,
+                loss_func = huber_loss,
+                batch_size = 128,
+                update_freq = 128,
+                update_horizon = 0,
+                min_replay_history = 1000,
+                target_update_freq = 1000,
+                rng = rng,
+            ),
+            explorer = EpsilonGreedyExplorer(
+                kind = :linear,
+                ϵ_init = 0.06,
+                ϵ_stable = 0.001,
+                decay_steps = 3_000_000,
+                rng = rng,
+            ),
+        ),
+        trajectory = CircularArraySARTTrajectory(
+            capacity = 200_000,
+            state = Vector{Float64} => (ns, )
+        ),
+    )
+
+    sl_agent = Agent(
+        policy = BehaviorCloningPolicy(;
+            approximator = NeuralNetworkApproximator(
+                model = Chain(
+                        Dense(ns, 128, relu; init = glorot_normal(rng)),
+                        Dense(128, na; init = glorot_normal(rng))
+                    ) |> cpu,
+                optimizer = Descent(0.01),
+            ),
+            explorer = WeightedSoftmaxExplorer(),
             batch_size = 128,
-            learn_freq = 128,
-            min_buffer_size_to_learn = 1000,
-            hidden_layers = (128),
-        
-            # Reinforcement Learning(RL) agent parameters
-            rl_loss_func = mse,
-            rl_learning_rate = 0.01,
-            replay_buffer_capacity = 200_000,
-            ϵ_start = 0.06,
-            ϵ_end = 0.001,
-            ϵ_decay = 3_000_000,
-            discount_factor = 1.0f0,
-            update_target_network_freq = 1000,
-        
-            # Supervisor Learning(SL) agent parameters
-            sl_learning_rate = 0.01,
-            reservoir_buffer_capacity = 2_000_000,
-            )
+            min_reservoir_history = 1000,
+            rng = rng,
+        ),
+        trajectory = ReservoirTrajectory(
+            2_000_000;# capacity
+            rng = rng,
+            :state => Vector{Float64},
+            :action_probs => Vector{Float64},
+        ),
+    )
+
+    # set parameters
+    η = 0.1 # anticipatory parameter
+    nfsp = NFSPAgentManager(
+        Dict(
+            (player, NFSPAgent(
+                deepcopy(rl_agent),
+                deepcopy(sl_agent),
+                η,
+                rng,
+                128, # update_freq for NFSPAgent
+                0, # initial step_counter
+                true, # initial NFSPAgent's learn mode
+            )) for player in players(wrapped_env) if player != chance_player(wrapped_env)
+        )
+    )
 
     stop_condition = StopAfterEpisode(3_000_000, is_show_progress=!haskey(ENV, "CI"))
     hook = ResultNEpisode(0, 10_000, [], [])
@@ -76,7 +133,6 @@ function RL.Experiment(
 end
 
 #+ tangle=false
-ENV["GKSwstype"]="nul" 
 using Plots
 ex = E`JuliaRL_NFSP_KuhnPoker`
 run(ex)
