6dcc7b5e0af1363fe7298046129e9e479886a8f4
Mobius1D <prsdhsk@gmail.com>
2021-07-26 19:40:08 +0530

    comment

--- src/ReinforcementLearningEnvironments/src/environments/3rd_party/gym.jl
+++ src/ReinforcementLearningEnvironments/src/environments/3rd_party/gym.jl
@@ -137,7 +137,7 @@ function list_gym_env_names(;
         "d4rl.gym_mujoco.gym_envs",
         "d4rl.locomotion.ant",
         "d4rl.gym_bullet.gym_envs",
-        "d4rl.pointmaze_bullet.bullet_maze",
+        "d4rl.pointmaze_bullet.bullet_maze", # yet to include flow and carls
     ],
 )   
     if PyCall.pyexists("d4rl") pyimport("d4rl") end
