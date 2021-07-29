1b68e18d21dceca2f9227c696103c543f958eabb
Jun Tian <tianjun.cpp@gmail.com>
2021-07-29 11:48:19 +0800

    fix some dead links

--- src/ReinforcementLearningCore/src/ReinforcementLearningCore.jl
+++ src/ReinforcementLearningCore/src/ReinforcementLearningCore.jl
@@ -5,8 +5,8 @@ using ReinforcementLearningBase
 const RLCore = ReinforcementLearningCore
 
 @doc """
-[ReinforcementLearningCore.jl](https://github.com/JuliaReinforcementLearning/ReinforcementLearningCore.jl) (**RLCore**)
-provides some standard and reusable components defined by [**RLBase**](https://github.com/JuliaReinforcementLearning/ReinforcementLearningBase.jl), hoping that they are useful for people to implement and experiment with different kinds of algorithms.
+[ReinforcementLearningCore.jl](https://juliareinforcementlearning.org/docs/rlcore/) (**RLCore**)
+provides some standard and reusable components defined by [**RLBase**](https://juliareinforcementlearning.org/docs/rlbase/), hoping that they are useful for people to implement and experiment with different kinds of algorithms.
 """ RLCore
 
 export RLCore
