a736254d840546167343c4da92486c7379b32cff
Mobius1D <prsdhsk@gmail.com>
2021-08-18 11:11:11 +0530

    Remove toy_text envs

--- src/ReinforcementLearningEnvironments/test/environments/3rd_party/gym.jl
+++ src/ReinforcementLearningEnvironments/test/environments/3rd_party/gym.jl
@@ -3,12 +3,9 @@
         modules = [
             "gym.envs.algorithmic",
             "gym.envs.classic_control",
-            "gym.envs.toy_text",
             "gym.envs.unittest",
         ],
     )  # mujoco, box2d, robotics are not tested here
-
-    gym_env_names = filter(x -> !(x in ["KellyCoinflipGeneralized-v0", "Blackjack-v0", "KellyCoinflip-v0"]), gym_env_names)  # not sure why this env has outliers
 
     for x in gym_env_names
         env = GymEnv(x)
