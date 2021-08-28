e3f3839c4c4e8041b03eade938ebd5bfa026c8b8
peter <csherry1231@gmail.com>
2021-08-28 10:56:46 +0800

    minor update

--- src/ReinforcementLearningZoo/src/algorithms/policy_gradient/maddpg.jl
+++ src/ReinforcementLearningZoo/src/algorithms/policy_gradient/maddpg.jl
@@ -95,8 +95,7 @@ function RLBase.update!(π::MADDPGManager, env::AbstractEnv)
         ρ = p.ρ
 
         # by default A, C, Aₜ, Cₜ on the same device.
-        _device(x) = send_to_device(device(A), x)
-        batches, s, a, s′ = _device((batches, s, a, s′))
+        batches, s, a, s′ = send_to_device(device(A), (batches, s, a, s′))
         r = batches[player][:reward]
         t = batches[player][:terminal]
         # for training behavior_actor.
