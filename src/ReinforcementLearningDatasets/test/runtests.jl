1e2deca05ee54bfd0e8cd6034e311cd9f37f2f1e
Prasidh Srikumar <prsdhsk@gmail.com>
2021-09-11 23:20:58 +0530

    remove deep_ope test

--- src/ReinforcementLearningDatasets/test/runtests.jl
+++ src/ReinforcementLearningDatasets/test/runtests.jl
@@ -11,6 +11,6 @@ ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"
     include("rl_unplugged_atari.jl")
     include("bsuite.jl")
     include("rl_unplugged_dm.jl")
-    include("deep_ope_d4rl.jl")
+    # include("deep_ope_d4rl.jl")
     # include("atari_dataset.jl")
 end
