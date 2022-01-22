14c13d36d5cab096abe00a19bfed34c75e8bc1bc
Andrea PIERRÉ <6053592+kir0ul@users.noreply.github.com>
2022-01-21 20:05:33 -0500

    docs: fix broken plot link

--- src/ReinforcementLearningCore/src/policies/q_based_policies/explorers/epsilon_greedy_explorer.jl
+++ src/ReinforcementLearningCore/src/policies/q_based_policies/explorers/epsilon_greedy_explorer.jl
@@ -29,16 +29,12 @@ Two kinds of epsilon-decreasing strategy are implemented here (`linear` and `exp
 # Example
 
 ```julia
-s = EpsilonGreedyExplorer(kind=:linear, ϵ_init=0.9, ϵ_stable=0.1, warmup_steps=100, decay_steps=100)
-plot([RLCore.get_ϵ(s, i) for i in 1:500], label="linear epsilon")
+s_lin = EpsilonGreedyExplorer(kind=:linear, ϵ_init=0.9, ϵ_stable=0.1, warmup_steps=100, decay_steps=100)
+plot([RLCore.get_ϵ(s_lin, i) for i in 1:500], label="linear epsilon")
+s_exp = EpsilonGreedyExplorer(kind=:exp, ϵ_init=0.9, ϵ_stable=0.1, warmup_steps=100, decay_steps=100)
+plot!([RLCore.get_ϵ(s_exp, i) for i in 1:500], label="exp epsilon")
 ```
-![](https://github.com/JuliaReinforcementLearning/ReinforcementLearning.jl/raw/master/docs/src/assets/img/linear_epsilon_greedy_selector.png)
-
-```julia
-s = EpsilonGreedyExplorer(kind=:exp, ϵ_init=0.9, ϵ_stable=0.1, warmup_steps=100, decay_steps=100)
-plot!([RLCore.get_ϵ(s, i) for i in 1:500], label="exp epsilon")
-```
-![](https://github.com/JuliaReinforcementLearning/ReinforcementLearning.jl/raw/master/docs/src/assets/img/exp_epsilon_greedy_selector.png)
+![](https://github.com/JuliaReinforcementLearning/ReinforcementLearning.jl/raw/master/docs/src/assets/epsilon_greedy_selector.png)
 """
 mutable struct EpsilonGreedyExplorer{Kind,IsBreakTie,R} <: AbstractExplorer
     ϵ_stable::Float64
