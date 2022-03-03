fefd141a0bee0ae4964b5f9141b2dbbd102e72a2
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2022-03-03 18:15:27 +0100

    add unit tests for GaussianNetwork

--- src/ReinforcementLearningCore/test/components/approximators.jl
+++ src/ReinforcementLearningCore/test/components/approximators.jl
@@ -47,4 +47,93 @@
         ac.actor(A)
         ac.critic(A)
     end
+
+    @testset "GaussianNetwork" begin
+        @testset "identity normalizer" begin
+            pre = Dense(20,15)
+            μ = Dense(15,10)
+            logσ = Dense(15,10)
+            gn = GaussianNetwork(pre, μ, logσ, identity)
+            @test Flux.params(gn) == Flux.Params([pre.W, pre.b, μ.W, μ.b, logσ.W, logσ.b])
+            state = rand(20,3) #batch of 3 states
+            m, s = gn(state)
+            @test size(m) == size(s) == (10,3)
+            a, logp = gn(state, is_sampling = true, is_return_log_prob = true)
+            @test size(a) == (10,3)
+            @test size(logp) == (1,3)
+            @test logp == sum(normlogpdf(m, exp.(s), a) .- (2.0f0 .* (log(2.0f0) .- a .- softplus.(-2.0f0 .* a))), dims = 1)
+            @test logp == gn(state, a)
+            as, logps = gn(Flux.unsqueeze(state,2), 5) #sample 5 actions
+            @test size(as) == (10,5,3)
+            @test size(logps) == (1,5,3)
+            logps2 = gn(Flux.unsqueeze(state,2), as)
+            @test logps2 == logps
+            g = Flux.gradient(Flux.params(gn)) do 
+                a, logp = gn(state, is_sampling = true, is_return_log_prob = true)
+                sum(logp)
+            end
+            g2 = Flux.gradient(Flux.params(gn)) do 
+                logp = gn(state, a)
+                sum(logp)
+            end
+        end
+        @testset "tanh normalizer" begin
+            pre = Dense(20,15)
+            μ = Dense(15,10)
+            logσ = Dense(15,10)
+            gn = GaussianNetwork(pre, μ, logσ)
+            @test Flux.params(gn) == Flux.Params([pre.W, pre.b, μ.W, μ.b, logσ.W, logσ.b])
+            state = rand(20,3) #batch of 3 states
+            m, s = gn(state)
+            @test size(m) == size(s) == (10,3)
+            a, logp = gn(state, is_sampling = true, is_return_log_prob = true)
+            @test size(a) == (10,3)
+            @test size(logp) == (1,3)
+            @test logp == sum(normlogpdf(m, exp.(s), a) .- (2.0f0 .* (log(2.0f0) .- a .- softplus.(-2.0f0 .* a))), dims = 1)
+            @test logp == gn(state, a) #this was broken
+            as, logps = gn(Flux.unsqueeze(state,2), 5) #sample 5 actions
+            @test size(as) == (10,5,3)
+            @test size(logps) == (1,5,3)
+            logps2 = gn(Flux.unsqueeze(state,2), as)
+            @test logps2 == logps
+            g = Flux.gradient(Flux.params(gn)) do 
+                a, logp = gn(state, is_sampling = true, is_return_log_prob = true)
+                sum(logp)
+            end
+            g2 = Flux.gradient(Flux.params(gn)) do 
+                logp = gn(state, a)
+                sum(logp)
+            end
+        end
+        @testset "CUDA" begin
+            if CUDA.is_functional()
+                pre = Dense(20,15) |> gpu
+                μ = Dense(15,10) |> gpu
+                logσ = Dense(15,10) |> gpu
+                gn = GaussianNetwork(pre, μ, logσ)
+                @test Flux.params(gn) == Flux.Params([pre.W, pre.b, μ.W, μ.b, logσ.W, logσ.b])
+                state = rand(20,3)  |> gpu #batch of 3 states
+                m, s = gn(state)
+                @test size(m) == size(s) == (10,3)
+                a, logp = gn(state, is_sampling = true, is_return_log_prob = true)
+                @test size(a) == (10,3)
+                @test size(logp) == (1,3)
+                @test logp == sum(normlogpdf(m, exp.(s), a) .- (2.0f0 .* (log(2.0f0) .- a .- softplus.(-2.0f0 .* a))), dims = 1)
+                @test logp == gn(state, a) #this was broken
+                as, logps = gn(Flux.unsqueeze(state,2), 5) #sample 5 actions
+                @test size(as) == (10,5,3)
+                @test size(logps) == (1,5,3)
+                logps2 = gn(Flux.unsqueeze(state,2), as)
+                @test logps2 == logps
+                g = Flux.gradient(Flux.params(gn)) do 
+                    a, logp = gn(state, is_sampling = true, is_return_log_prob = true)
+                    sum(logp)
+                end
+                g2 = Flux.gradient(Flux.params(gn)) do 
+                    logp = gn(state, a)
+                    sum(logp)
+                end
+            end
+        end
+    end
 end
