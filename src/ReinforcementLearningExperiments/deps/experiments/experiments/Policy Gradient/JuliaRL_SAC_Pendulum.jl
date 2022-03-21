e90240c587ba22c68c01ae78b743f29a447b8314
Jun Tian <find_my_way@foxmail.com>
2022-03-21 14:11:48 +0800

    fix #605 (#606)
    * fix #605
    
    * update NEWs
    
    * use latest Flux.jl

--- src/ReinforcementLearningExperiments/deps/experiments/experiments/Policy Gradient/JuliaRL_SAC_Pendulum.jl
+++ src/ReinforcementLearningExperiments/deps/experiments/experiments/Policy Gradient/JuliaRL_SAC_Pendulum.jl
@@ -12,17 +12,18 @@ using StableRNGs
 using Flux
 using Flux.Losses
 using IntervalSets
+using CUDA
 
 function RL.Experiment(
     ::Val{:JuliaRL},
     ::Val{:SAC},
     ::Val{:Pendulum},
     ::Nothing;
-    save_dir = nothing,
-    seed = 123,
+    save_dir=nothing,
+    seed=123
 )
     rng = StableRNG(seed)
-    inner_env = PendulumEnv(T = Float32, rng = rng)
+    inner_env = PendulumEnv(T=Float32, rng=rng)
     action_dims = inner_env.n_actions
     A = action_space(inner_env)
     low = A.left
@@ -32,55 +33,56 @@ function RL.Experiment(
 
     env = ActionTransformedEnv(
         inner_env;
-        action_mapping = x -> low + (x[1] + 1) * 0.5 * (high - low),
+        action_mapping=x -> low + (x[1] + 1) * 0.5 * (high - low)
     )
     init = glorot_uniform(rng)
 
     create_policy_net() = NeuralNetworkApproximator(
-        model = GaussianNetwork(
-            pre = Chain(
-                Dense(ns, 30, relu, init = init), 
-                Dense(30, 30, relu, init = init),
+        model=GaussianNetwork(
+            pre=Chain(
+                Dense(ns, 30, relu, init=init),
+                Dense(30, 30, relu, init=init),
             ),
-            μ = Chain(Dense(30, na, init = init)),
-            logσ = Chain(Dense(30, na, x -> clamp(x, typeof(x)(-10), typeof(x)(2)), init = init)),
+            μ=Chain(Dense(30, na, init=init)),
+            logσ=Chain(Dense(30, na, x -> clamp(x, typeof(x)(-10), typeof(x)(2)), init=init)),
         ),
-        optimizer = ADAM(0.003),
+        optimizer=ADAM(0.003),
     ) |> gpu
 
     create_q_net() = NeuralNetworkApproximator(
-        model = Chain(
-            Dense(ns + na, 30, relu; init = init),
-            Dense(30, 30, relu; init = init),
-            Dense(30, 1; init = init),
+        model=Chain(
+            Dense(ns + na, 30, relu; init=init),
+            Dense(30, 30, relu; init=init),
+            Dense(30, 1; init=init),
         ),
-        optimizer = ADAM(0.003),
+        optimizer=ADAM(0.003),
     ) |> gpu
 
     agent = Agent(
-        policy = SACPolicy(
-            policy = create_policy_net(),
-            qnetwork1 = create_q_net(),
-            qnetwork2 = create_q_net(),
-            target_qnetwork1 = create_q_net(),
-            target_qnetwork2 = create_q_net(),
-            γ = 0.99f0,
-            τ = 0.005f0,
-            α = 0.2f0,
-            batch_size = 64,
-            start_steps = 1000,
-            start_policy = RandomPolicy(Space([-1.0..1.0 for _ in 1:na]); rng = rng),
-            update_after = 1000,
-            update_freq = 1,
-            automatic_entropy_tuning = true,
-            lr_alpha = 0.003f0,
-            action_dims = action_dims,
-            rng = rng,
+        policy=SACPolicy(
+            policy=create_policy_net(),
+            qnetwork1=create_q_net(),
+            qnetwork2=create_q_net(),
+            target_qnetwork1=create_q_net(),
+            target_qnetwork2=create_q_net(),
+            γ=0.99f0,
+            τ=0.005f0,
+            α=0.2f0,
+            batch_size=64,
+            start_steps=1000,
+            start_policy=RandomPolicy(Space([-1.0 .. 1.0 for _ in 1:na]); rng=rng),
+            update_after=1000,
+            update_freq=1,
+            automatic_entropy_tuning=true,
+            lr_alpha=0.003f0,
+            action_dims=action_dims,
+            rng=rng,
+            device_rng=CUDA.functional() ? CUDA.CURAND.RNG() : rng
         ),
-        trajectory = CircularArraySARTTrajectory(
-            capacity = 10000,
-            state = Vector{Float32} => (ns,),
-            action = Vector{Float32} => (na,),
+        trajectory=CircularArraySARTTrajectory(
+            capacity=10000,
+            state=Vector{Float32} => (ns,),
+            action=Vector{Float32} => (na,),
         ),
     )
 
--- src/ReinforcementLearningZoo/src/algorithms/policy_gradient/sac.jl
+++ src/ReinforcementLearningZoo/src/algorithms/policy_gradient/sac.jl
@@ -6,6 +6,7 @@ mutable struct SACPolicy{
     BC2<:NeuralNetworkApproximator,
     P,
     R<:AbstractRNG,
+    DR<:AbstractRNG,
 } <: AbstractPolicy
     policy::BA
     qnetwork1::BC1
@@ -25,6 +26,7 @@ mutable struct SACPolicy{
     target_entropy::Float32
     update_step::Int
     rng::R
+    device_rng::DR
     # Logging
     reward_term::Float32
     entropy_term::Float32
@@ -52,7 +54,8 @@ end
 - `lr_alpha::Float32 = 0.003f0`, learning rate of tuning entropy.
 - `action_dims = 0`, the dimensionality of the action. if `automatic_entropy_tuning = true`, must enter this parameter.
 - `update_step = 0`,
-- `rng = Random.GLOBAL_RNG`,
+- `rng = Random.GLOBAL_RNG`, used to sample batch from trajectory or action from action distribution.
+- `device_rng = Random.GLOBAL_RNG`, should be set to `CUDA.CURAND.RNG()` if the `policy` is set to work with `CUDA.jl`
 
 `policy` is expected to output a tuple `(μ, logσ)` of mean and
 log standard deviations for the desired action distributions, this
@@ -64,21 +67,22 @@ function SACPolicy(;
     policy,
     qnetwork1,
     qnetwork2,
-    target_qnetwork1 = deepcopy(qnetwork1),
-    target_qnetwork2 = deepcopy(qnetwork2),
-    γ = 0.99f0,
-    τ = 0.005f0,
-    α = 0.2f0,
-    batch_size = 32,
-    start_steps = 10000,
-    update_after = 1000,
-    update_freq = 50,
-    automatic_entropy_tuning = true,
-    lr_alpha = 0.003f0,
-    action_dims = 0,
-    update_step = 0,
-    start_policy = update_step == 0 ? identity : policy,
-    rng = Random.GLOBAL_RNG,
+    target_qnetwork1=deepcopy(qnetwork1),
+    target_qnetwork2=deepcopy(qnetwork2),
+    γ=0.99f0,
+    τ=0.005f0,
+    α=0.2f0,
+    batch_size=32,
+    start_steps=10000,
+    update_after=1000,
+    update_freq=50,
+    automatic_entropy_tuning=true,
+    lr_alpha=0.003f0,
+    action_dims=0,
+    update_step=0,
+    start_policy=update_step == 0 ? identity : policy,
+    rng=Random.GLOBAL_RNG,
+    device_rng=Random.GLOBAL_RNG
 )
     copyto!(qnetwork1, target_qnetwork1)  # force sync
     copyto!(qnetwork2, target_qnetwork2)  # force sync
@@ -104,8 +108,9 @@ function SACPolicy(;
         Float32(-action_dims),
         update_step,
         rng,
-        0f0,
-        0f0,
+        device_rng,
+        0.0f0,
+        0.0f0,
     )
 end
 
@@ -120,7 +125,7 @@ function (p::SACPolicy)(env)
         s = send_to_device(D, state(env))
         s = Flux.unsqueeze(s, ndims(s) + 1)
         # trainmode:
-        action = dropdims(p.policy(p.rng, s; is_sampling=true), dims=2) # Single action vec, drop second dim
+        action = dropdims(p.policy(p.device_rng, s; is_sampling=true), dims=2) # Single action vec, drop second dim
         send_to_host(action)
 
         # testmode:
@@ -147,7 +152,7 @@ function RLBase.update!(p::SACPolicy, batch::NamedTuple{SARTS})
 
     γ, τ, α = p.γ, p.τ, p.α
 
-    a′, log_π = p.policy(p.rng, s′; is_sampling=true, is_return_log_prob=true)
+    a′, log_π = p.policy(p.device_rng, s′; is_sampling=true, is_return_log_prob=true)
     q′_input = vcat(s′, a′)
     q′ = min.(p.target_qnetwork1(q′_input), p.target_qnetwork2(q′_input))
 
@@ -169,12 +174,12 @@ function RLBase.update!(p::SACPolicy, batch::NamedTuple{SARTS})
 
     # Train Policy
     p_grad = gradient(Flux.params(p.policy)) do
-        a, log_π = p.policy(p.rng, s; is_sampling=true, is_return_log_prob=true)
+        a, log_π = p.policy(p.device_rng, s; is_sampling=true, is_return_log_prob=true)
         q_input = vcat(s, a)
         q = min.(p.qnetwork1(q_input), p.qnetwork2(q_input))
         reward = mean(q)
         entropy = mean(log_π)
-        ignore() do 
+        ignore() do
             p.reward_term = reward
             p.entropy_term = entropy
         end
