1627fb8b323f82a50d998d7aff8642d0790a8039
Jun Tian <tianjun.cpp@gmail.com>
2022-05-08 23:34:24 +0800

    finish agent.jl

--- src/ReinforcementLearningCore/src/policies/agents/agent.jl
+++ src/ReinforcementLearningCore/src/policies/agents/agent.jl
@@ -1,10 +1,11 @@
 export Agent
 
+using Base.Threads
 import Functors: functor
 using Setfield: @set
 
 """
-    Agent(;kwargs...)
+    Agent(;policy, trajectory)
 
 A wrapper of an `AbstractPolicy`. Generally speaking, it does nothing but to
 update the trajectory and policy appropriately in different stages.
@@ -12,16 +13,24 @@ update the trajectory and policy appropriately in different stages.
 # Keywords & Fields
 
 - `policy`::[`AbstractPolicy`](@ref): the policy to use
-- `trajectory`::[`AbstractTrajectory`](@ref): used to store transitions between an agent and an environment
+- `trajectory`::[`Trajectory`](@ref): used to store intractions between an agent and an environment
 """
 Base.@kwdef struct Agent{P,T} <: AbstractPolicy
     policy::P
     trajectory::T
-    function Agent(p, t) end
+
+    function Agent(p::P, t::T) where {P,T}
+        agent = new{P,T}(p, t)
+        bind(t, @spawn(optimise!(agent)))
+        agent
+    end
 end
 
 functor(x::Agent) = (policy = x.policy,), y -> @set x.policy = y.policy
 
+# !!! TODO: In async scenarios, parameters of the policy may still be updating
+# (partially), which will result to incorrect action. This should be addressed
+# in Oolong.jl with a wrapper
 (agent::Agent)(env) = agent.policy(env)
 
 (agent::Agent)(::PreActStage, env, action) =
@@ -30,13 +39,15 @@ functor(x::Agent) = (policy = x.policy,), y -> @set x.policy = y.policy
 (agent::Agent)(::PostActStage, env) =
     push!(agent.trajectory; reward = reward(env), terminal = is_terminated(env))
 
-function (agent::Agent{P,<:Trajectory})(::PreActStage, env, action) where {P}
+function (agent::Agent)(::PreActStage, env, action)
     push!(agent.trajectory; state = state(env), action = action)
-    optimise!(agent.policy, agent.trajectory)
+    if TrajectoryStyle(agent.trajectory) === SyncTrajectoryStyle()
+        optimise!(agent)
+    end
 end
 
-function optimise!(p::AbstractPolicy, t::AbstractTrajectory)
-    for batch in t
-        optimise!(p, batch)
+function optimise!(agent::Agent)
+    for batch in agent.trajectory
+        optimise!(agent.policy, batch)
     end
 end
