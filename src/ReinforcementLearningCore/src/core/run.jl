c01c9526590916aef6df68d8ed93afd27926f80b
Jun Tian <tianjun.cpp@gmail.com>
2022-06-03 10:27:46 +0800

    add Experiment

--- src/ReinforcementLearningCore/src/core/run.jl
+++ src/ReinforcementLearningCore/src/core/run.jl
@@ -1,4 +1,64 @@
-import Base: run
+export @E_cmd
+
+
+using Parsers
+
+macro E_cmd(s)
+    Experiment(s)
+end
+
+function try_parse(s, TS = (Bool, Int, Float32, Float64))
+    if s == "nothing"
+        nothing
+    else
+        for T in TS
+            res = Parsers.tryparse(T, s)
+            if !isnothing(res)
+                return res
+            end
+        end
+        s
+    end
+end
+
+function try_parse_kw(s)
+    kw = []
+    # !!! obviously, it's not correct when a value is string and contains ","
+    for part in split(s, ",")
+        kv = split(part, "=")
+        @assert length(kv) == 2
+        k, v = kv
+        push!(kw, Symbol(strip(k)) => try_parse(strip(v)))
+    end
+    NamedTuple(kw)
+end
+
+function Experiment(s::String)
+    m = match(r"(?<source>\w+)_(?<method>\w+)_(?<env>\w+)(\((?<game>.*)\))?", s)
+    isnothing(m) && throw(
+        ArgumentError(
+            "invalid format, got $s, expected format is JuliaRL_DQN_Atari(game=\"pong\")`",
+        ),
+    )
+    source = m[:source]
+    method = m[:method]
+    env = m[:env]
+    kw_args = isnothing(m[:game]) ? (;) : try_parse_kw(m[:game])
+    Experiment(Val(source), Val(method), Val(env); kw_args...)
+end
+
+
+struct Experiment
+    policy_factory::Any
+    env_factory::Any
+    stop_condition_factory::Any
+    hook_factory::Any
+end
+
+(ex::Experiment)() =
+    (ex.policy_factory(), ex.env_factory(), ex.stop_condition_factory(), ex.hook_factory())
+
+run(ex::Experiment) = run(ex()...)
 
 function run(
     policy::AbstractPolicy,
