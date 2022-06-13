c70f4f057202d6e02a39399458160002756b63ed
Jun Tian <find_my_way@foxmail.com>
2022-06-13 14:38:59 +0800

    enable OpenSpiel (#691)
    * enable OpenSpiel
    
    * passCI

--- src/ReinforcementLearningEnvironments/test/environments/3rd_party/open_spiel.jl
+++ src/ReinforcementLearningEnvironments/test/environments/3rd_party/open_spiel.jl
@@ -1,12 +1,11 @@
 @testset "OpenSpielEnv" begin
-
-    # for name in [
-    #     "tic_tac_toe",
-    #     "kuhn_poker",
-    #     "goofspiel(imp_info=True,num_cards=4,points_order=descending)",
-    # ]
-    #     @info "testing OpenSpiel: $name"
-    #     env = OpenSpielEnv(name)
-    #     RLBase.test_runnable!(env)
-    # end
+    for name in [
+        "tic_tac_toe",
+        "kuhn_poker",
+        "goofspiel(imp_info=True,num_cards=4,points_order=descending)",
+    ]
+        @info "testing OpenSpiel: $name"
+        env = OpenSpielEnv(name)
+        RLBase.test_runnable!(env)
+    end
 end
