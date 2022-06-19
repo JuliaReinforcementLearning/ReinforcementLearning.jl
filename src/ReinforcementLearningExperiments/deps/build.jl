b4ccd43e03ac9fc32b5e59223c1f533a2c038652
Jun Tian <tianjun.cpp@gmail.com>
2022-06-19 22:23:57 +0800

    add more debug info

--- src/ReinforcementLearningExperiments/deps/build.jl
+++ src/ReinforcementLearningExperiments/deps/build.jl
@@ -1,11 +1,12 @@
 using Weave
 
-const DEST_DIR = joinpath(@__DIR__, "..", "src", "experiments")
-
 for (root, dirs, files) in walkdir(joinpath(@__DIR__, "experiments"))
     for f in files
         if splitext(f)[2] == ".jl"
-            tangle(joinpath(root,f);informat="script", out_path=DEST_DIR)
+            src = joinpath(root, f)
+            dest_dir = joinpath(@__DIR__, "..", "src", "experiments")
+            @info "extracting source code from $src to $dest_dir"
+            tangle(src; informat="script", out_path=dest_dir)
         end
     end
 end
