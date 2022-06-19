0d9032aa43a0a82ccfb4877cd0f5d40fd8cf742b
Jun Tian <find_my_way@foxmail.com>
2022-06-19 12:39:39 +0800

    add PrioritizedDQN (#698)
    * add PrioritizedDQN
    
    * add test for PrioritizedDQN

--- src/ReinforcementLearningExperiments/deps/experiments/experiments/DQN/JuliaRL_PrioritizedDQN_CartPole.jl
+++ src/ReinforcementLearningExperiments/deps/experiments/experiments/DQN/JuliaRL_PrioritizedDQN_CartPole.jl
@@ -2,7 +2,7 @@
 # title: JuliaRL\_PrioritizedDQN\_CartPole
 # cover: assets/JuliaRL_PrioritizedDQN_CartPole.png
 # description: PrioritizedDQN applied to CartPole
-# date: 2021-05-22
+# date: 2022-06-18
 # author: "[Jun Tian](https://github.com/findmyway)"
 # ---
 
@@ -16,59 +16,68 @@ function RL.Experiment(
     ::Val{:JuliaRL},
     ::Val{:PrioritizedDQN},
     ::Val{:CartPole},
-    ::Nothing;
-    save_dir = nothing,
-    seed = 123,
+    ; seed=123,
+    n=1,
+    γ=0.99f0,
+    is_enable_double_DQN=true
 )
     rng = StableRNG(seed)
 
-    env = CartPoleEnv(; T = Float32, rng = rng)
+    env = CartPoleEnv(; T=Float32, rng=rng)
     ns, na = length(state(env)), length(action_space(env))
 
     agent = Agent(
-        policy = QBasedPolicy(
-            learner = PrioritizedDQNLearner(
-                approximator = NeuralNetworkApproximator(
-                    model = Chain(
-                        Dense(ns, 128, relu; init = glorot_uniform(rng)),
-                        Dense(128, 128, relu; init = glorot_uniform(rng)),
-                        Dense(128, na; init = glorot_uniform(rng)),
-                    ) |> gpu,
-                    optimizer = ADAM(),
+        policy=QBasedPolicy(
+            learner=PrioritizedDQNLearner(
+                approximator=Approximator(
+                    model=TwinNetwork(
+                        Chain(
+                            Dense(ns, 128, relu; init=glorot_uniform(rng)),
+                            Dense(128, 128, relu; init=glorot_uniform(rng)),
+                            Dense(128, na; init=glorot_uniform(rng)),
+                        );
+                        sync_freq=100
+                    ),
+                    optimiser=ADAM(),
                 ),
-                target_approximator = NeuralNetworkApproximator(
-                    model = Chain(
-                        Dense(ns, 128, relu; init = glorot_uniform(rng)),
-                        Dense(128, 128, relu; init = glorot_uniform(rng)),
-                        Dense(128, na; init = glorot_uniform(rng)),
-                    ) |> gpu,
-                    optimizer = ADAM(),
-                ),
-                loss_func = (ŷ, y) -> huber_loss(ŷ, y; agg = identity),
-                stack_size = nothing,
-                batch_size = 32,
-                update_horizon = 1,
-                min_replay_history = 100,
-                update_freq = 1,
-                target_update_freq = 100,
-                rng = rng,
+                n=n,
+                γ=γ,
+                β_priority=0.5f0,
+                is_enable_double_DQN=is_enable_double_DQN,
+                loss_func=(ŷ, y) -> huber_loss(ŷ, y; agg=identity),
+                rng=rng,
             ),
-            explorer = EpsilonGreedyExplorer(
-                kind = :exp,
-                ϵ_stable = 0.01,
-                decay_steps = 500,
-                rng = rng,
+            explorer=EpsilonGreedyExplorer(
+                kind=:exp,
+                ϵ_stable=0.01,
+                decay_steps=500,
+                rng=rng,
             ),
         ),
-        trajectory = CircularArrayPSARTTrajectory(
-            capacity = 1000,
-            state = Vector{Float32} => (ns,),
-        ),
+        trajectory=Trajectory(
+            container=CircularPrioritizedTraces(
+                CircularArraySARTTraces(
+                    capacity=1000,
+                    state=Float32 => (ns,),
+                );
+                default_priority=100.0f0
+            ),
+            sampler=NStepBatchSampler{SS′ART}(
+                n=n,
+                γ=γ,
+                batch_size=32,
+                rng=rng
+            ),
+            controller=InsertSampleRatioController(
+                threshold=100,
+                n_inserted=-1
+            )
+        )
     )
 
     stop_condition = StopAfterStep(10_000)
     hook = TotalRewardPerEpisode()
-    Experiment(agent, env, stop_condition, hook, "# Play CartPole with PrioritizedDQN")
+    Experiment(agent, env, stop_condition, hook)
 end
 
 
--- src/ReinforcementLearningExperiments/src/ReinforcementLearningExperiments.jl
+++ src/ReinforcementLearningExperiments/src/ReinforcementLearningExperiments.jl
@@ -10,6 +10,7 @@ const EXPERIMENTS_DIR = joinpath(@__DIR__, "experiments")
 # end
 include(joinpath(EXPERIMENTS_DIR, "JuliaRL_BasicDQN_CartPole.jl"))
 include(joinpath(EXPERIMENTS_DIR, "JuliaRL_DQN_CartPole.jl"))
+include(joinpath(EXPERIMENTS_DIR, "JuliaRL_PrioritizedDQN_CartPole.jl"))
 
 # dynamic loading environments
 function __init__() end
