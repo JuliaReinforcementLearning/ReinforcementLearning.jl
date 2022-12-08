d20c431d722997aa49ff85fef5e3800ae71477a3
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-12-08 14:15:53 +0100

    Fix TD Learner so that it handles MultiAgent/Simultaneous with NoOp (#769)

--- src/ReinforcementLearningZoo/src/algorithms/tabular/td_learner.jl
+++ src/ReinforcementLearningZoo/src/algorithms/tabular/td_learner.jl
@@ -91,7 +91,9 @@ function _update!(
     for i in 1:min(n + 1, length(R))
         G = R[end-i+1] + γ * G
         s, a = S[end-i], A[end-i]
-        update!(Q, (s, a) => Q(s, a) - G)
+        if !(a isa NoOp)
+            update!(Q, (s, a) => Q(s, a) - G)
+        end
     end
 end
 
@@ -149,8 +151,10 @@ function _update!(
 
     if length(R) >= n + 1
         s, a, s′ = S[end-n-1], A[end-n-1], S[end]
-        G = discount_rewards_reduced(@view(R[end-n:end]), γ) + γ^(n + 1) * maximum(Q(s′))
-        update!(Q, (s, a) => Q(s, a) - G)
+        if !(a isa NoOp)
+            G = discount_rewards_reduced(@view(R[end-n:end]), γ) + γ^(n + 1) * maximum(Q(s′))
+            update!(Q, (s, a) => Q(s, a) - G)
+        end
     end
 end
 
