6a4414b44ed4be19e9185b89c57560690c9cb6c9
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-03-08 12:56:23 +0100

    Fix hook tests

--- src/ReinforcementLearningCore/test/core/hooks.jl
+++ src/ReinforcementLearningCore/test/core/hooks.jl
@@ -5,12 +5,14 @@
 
     env = RandomWalk1D()
     env.pos = 7
+    policy = RandomPolicy(legal_action_space(env))
+
     for h in (h_1, h_2, h_3)
-        h(PostActStage, 1, env)
+        h(PostActStage(), policy, env)
         @test h.reward == 1
-        h(PostEpisodeStage, 1, env)
+        h(PostEpisodeStage(), policy, env)
         @test h.reward == 0
         @test h.rewards == [1]
-        h(PostExperimentStage, 1, env)
+        h(PostExperimentStage(), policy, env)
     end
 end
