2e7633ec26d13dd42320f5daee3b9e0a1bdfffa6
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-03-15 12:21:31 +0100

    Refactor offline experiments

--- src/ReinforcementLearningExperiments/deps/experiments/experiments/Offline/JuliaRL_FisherBRC_Pendulum.jl
+++ src/ReinforcementLearningExperiments/deps/experiments/experiments/Offline/JuliaRL_FisherBRC_Pendulum.jl
@@ -45,7 +45,7 @@ function RLCore.Experiment(
                 Dense(64, 64, relu),
             ),
             μ = Chain(Dense(64, na, init = init)),
-            logσ = Chain(Dense(64, na, init = init)),
+            σ = Chain(Dense(64, na, softplus, init = init)),
         ),
         optimizer = Adam(0.003),
     )
