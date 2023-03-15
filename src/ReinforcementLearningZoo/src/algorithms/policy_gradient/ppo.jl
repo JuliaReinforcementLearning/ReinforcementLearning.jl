a5ff3490362bb0b171fc3ff039f6c6c80b7c4c92
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-03-15 13:42:24 +0100

    Update src/ReinforcementLearningZoo/src/algorithms/policy_gradient/ppo.jl
    Co-authored-by: Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>

--- src/ReinforcementLearningZoo/src/algorithms/policy_gradient/ppo.jl
+++ src/ReinforcementLearningZoo/src/algorithms/policy_gradient/ppo.jl
@@ -1,5 +1,5 @@
 export PPOPolicy, PPOTrajectory, MaskedPPOTrajectory
-import ReinforcementLearningCore.normlogpdf
+import ReinforcementLearningCore: normlogpdf
 
 const PPOTrajectory = Trajectory{
     <:NamedTuple{
