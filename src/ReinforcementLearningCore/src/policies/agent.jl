6ffad39dd33195cdff5efa628bfbb0bed0c56aca
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-03-15 11:42:39 +0100

    Docstring

--- src/ReinforcementLearningCore/src/policies/agent.jl
+++ src/ReinforcementLearningCore/src/policies/agent.jl
@@ -8,7 +8,9 @@ using Functors: @functor
     Agent(;policy, trajectory) <: AbstractPolicy
 
 A wrapper of an `AbstractPolicy`. Generally speaking, it does nothing but to
-update the trajectory and policy appropriately in different stages.
+update the trajectory and policy appropriately in different stages. Agent
+is a Callable and its call method accepts varargs and keyword arguments to be
+passed to the policy. 
 
 """
 mutable struct Agent{P,T} <: AbstractPolicy
