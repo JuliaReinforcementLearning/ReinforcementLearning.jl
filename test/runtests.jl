509a2bf41661c950826388ea833ed1071b5e55ab
fchrstou <filippos.christou@ikr.uni-stuttgart.de>
2023-03-24 20:02:47 +0100

    Local total testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using Test
 using ReinforcementLearning
 
+const SRC = (@__DIR__)* "/../src/"
 @testset "ReinforcementLearning" begin
+    include(SRC*"/ReinforcementLearningCore/test/runtests.jl")
+    include(SRC*"/ReinforcementLearningBase/test/runtests.jl")
+    include(SRC*"/ReinforcementLearningZoo/test/runtests.jl")
+#    include(SRC*"/ReinforcementLearningEnvironments/test/runtests.jl")
+#    include(SRC*"/DistributedReinforcementLearning/test/runtests.jl")
 end
