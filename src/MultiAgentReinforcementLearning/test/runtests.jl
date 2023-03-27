de44ee587e1c33f44b84f87cfcaa14561f76c2fd
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-03-27 12:24:24 +0200

    Clean up tests

--- src/MultiAgentReinforcementLearning/test/runtests.jl
+++ src/MultiAgentReinforcementLearning/test/runtests.jl
@@ -1,4 +1,18 @@
 using Test
+using MultiAgentReinforcementLearning
+using StableRNGs
+using PyCall
+using ReinforcementLearningEnvironments
+using ReinforcementLearningBase
+using ReinforcementLearningCore
+using ReinforcementLearningZoo
+using Distributions
+using Flux
+using Flux: glorot_uniform
 
 
-include("tests.jl")
+@testset "MultiAgentReinforcementLearning" begin
+    include("independent_learner.jl")
+    include("maddpg_learner.jl")
+    include("tic_tac_toe.jl")
+end
