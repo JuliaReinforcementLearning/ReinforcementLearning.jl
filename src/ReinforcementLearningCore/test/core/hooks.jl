4de8c0637e605ce0cdd1ca1e8c906d2fac0294cf
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-03-28 17:07:55 +0200

    Also test that it

--- src/ReinforcementLearningCore/test/core/hooks.jl
+++ src/ReinforcementLearningCore/test/core/hooks.jl
@@ -72,7 +72,7 @@ end
 
 @testset "DoEveryNStep" begin
     h_1 = DoEveryNStep((hook, agent, env) -> (env.pos += 1); n=2)
-    h_2 = DoEveryNStep((hook, agent, env) -> (env.pos += 1); n=2)
+    h_2 = DoEveryNStep((hook, agent, env) -> (env.pos += 1); n=1)
 
     for h in (h_1, h_2)
         env = RandomWalk1D()
@@ -80,7 +80,7 @@ end
         policy = RandomPolicy(legal_action_space(env))
         for t = 1:4
             h(PostActStage(), policy, env)
-            @test env.pos == 1+ div(t,2)
+            @test env.pos == 1+ div(t,h.n)
         end
         test_noop!(h, stages=[PreActStage(), PreEpisodeStage(), PostEpisodeStage(), PreExperimentStage(), PostExperimentStage()])
     end
@@ -156,7 +156,7 @@ end
 @testset "DoEveryNEpisode" begin
     h_1 = DoEveryNEpisode((hook, agent, env) -> (env.pos += 1); n=2, stage=PreEpisodeStage())
     h_2 = DoEveryNEpisode((hook, agent, env) -> (env.pos += 1); n=2, stage=PostEpisodeStage())
-    h_3 = DoEveryNEpisode((hook, agent, env) -> (env.pos += 1); n=2)
+    h_3 = DoEveryNEpisode((hook, agent, env) -> (env.pos += 1); n=1)
     h_list = (h_1, h_2, h_3)
     stage_list = (PreEpisodeStage(), PostEpisodeStage(), PostEpisodeStage())
 
@@ -168,7 +168,7 @@ end
         policy = RandomPolicy(legal_action_space(env))
         for t in 1:4
             h(stage, policy, env)
-            @test env.pos == 1 + div(t,2)
+            @test env.pos == 1 + div(t,h.n)
         end     
         test_noop!(h, stages=[PreActStage(), PostActStage(), PreExperimentStage(), PostExperimentStage()])
     end
