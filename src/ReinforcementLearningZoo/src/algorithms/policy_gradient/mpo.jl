47a26f344d0882bfb7e441b306950928b56d14b8
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-03-28 17:23:41 +0200

    remove a problematic modulo

--- src/ReinforcementLearningZoo/src/algorithms/policy_gradient/mpo.jl
+++ src/ReinforcementLearningZoo/src/algorithms/policy_gradient/mpo.jl
@@ -107,7 +107,6 @@ end
 
 #Here we apply the TD3 Q network approach. The original MPO paper uses retrace.
 function update_critic!(p::MPOPolicy, batches)
-    modulo = rand(p.rng, (0,1)) #we randomize this so that if the number of batches is odd, we do not train one critic more than the other.
     for (id, batch) in enumerate(batches)
         s, s′, a, r, t, = send_to_device(device(p.qnetwork1), batch)
         γ, τ = p.γ, p.τ
@@ -120,7 +119,7 @@ function update_critic!(p::MPOPolicy, batches)
 
         # Train Q Networks
         q_input = vcat(s, a)
-        if id % 2 == modulo
+        if id % 2 == 0
             q_grad_1 = gradient(Flux.params(p.qnetwork1)) do
                 q1 = p.qnetwork1(q_input) |> vec
                 l = mse(q1, y)
