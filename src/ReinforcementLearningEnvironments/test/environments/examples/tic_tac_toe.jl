57f00d8c6a440773d1f907ec9e4fbfcb20a06771
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-04-24 15:42:46 +0200

    Env tests

--- src/ReinforcementLearningEnvironments/test/environments/examples/tic_tac_toe.jl
+++ src/ReinforcementLearningEnvironments/test/environments/examples/tic_tac_toe.jl
@@ -1,7 +1,14 @@
 @testset "TicTacToeEnv" begin
     using DomainSets
-    using ReinforcementLearningEnvironments, ReinforcementLearningBase
+    using ReinforcementLearningEnvironments, ReinforcementLearningBase, ReinforcementLearningCore
+
     env = TicTacToeEnv()
+    stop_condition = StopWhenDone()
+    multiagent_hook = MultiAgentHook((; Symbol(1) => StepsPerEpisode(), Symbol(2) => StepsPerEpisode()))
+    multiagent_policy = MultiAgentPolicy((;
+        Symbol(1) => Agent(RandomPolicy(), trajectory_1),
+        Symbol(2) => Agent(RandomPolicy(), trajectory_2),
+    ))
 
     RLBase.test_interfaces!(env)
     RLBase.test_runnable!(env)
@@ -12,5 +19,8 @@
     @test RLBase.state_space(env, Observation{BitArray{3}}(), Symbol(1)) isa ArrayProductDomain
     @test RLBase.state_space(env, Observation{String}(), Symbol(1)) isa DomainSets.FullSpace{String}
     @test RLBase.state(env, Observation{String}(), Symbol(1)) isa String
+
+    Base.run(multiagent_policy, env, stop_condition, multiagent_hook)
+    @test RLBase.legal_action_space_mask(env, Symbol(1)) == falses(9)
 end
 
