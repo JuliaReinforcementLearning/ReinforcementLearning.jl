5cb3bc60f6d03c7a365f7e7dbac35ea681d3e8e5
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-04-24 15:46:05 +0200

    add missing test

--- src/ReinforcementLearningEnvironments/test/environments/examples/tic_tac_toe.jl
+++ src/ReinforcementLearningEnvironments/test/environments/examples/tic_tac_toe.jl
@@ -19,8 +19,9 @@
     @test RLBase.state_space(env, Observation{BitArray{3}}(), Symbol(1)) isa ArrayProductDomain
     @test RLBase.state_space(env, Observation{String}(), Symbol(1)) isa DomainSets.FullSpace{String}
     @test RLBase.state(env, Observation{String}(), Symbol(1)) isa String
-
+    @test RLBase.state(env, Observation{String}()) isa String
     Base.run(multiagent_policy, env, stop_condition, multiagent_hook)
     @test RLBase.legal_action_space_mask(env, Symbol(1)) == falses(9)
 end
+
 
