5819fa669ac88e541575db0591780641dc30e44d
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-05-03 14:57:45 +0200

    Add back ComposedStop

--- src/ReinforcementLearningCore/src/core/stop_conditions.jl
+++ src/ReinforcementLearningCore/src/core/stop_conditions.jl
@@ -3,6 +3,28 @@ export StopAfterStep,
 
 import ProgressMeter
 
+
+#####
+# ComposedStopCondition
+#####
+
+"""
+    ComposedStopCondition(stop_conditions...; reducer = any)
+
+The result of `stop_conditions` is reduced by `reducer`.
+"""
+struct ComposedStopCondition{S,T}
+    stop_conditions::S
+    reducer::T
+    function ComposedStopCondition(stop_conditions...; reducer = any)
+        new{typeof(stop_conditions),typeof(reducer)}(stop_conditions, reducer)
+    end
+end
+
+function (s::ComposedStopCondition)(args...)
+    s.reducer(sc(args...) for sc in s.stop_conditions)
+end
+
 #####
 # StopAfterStep
 #####
