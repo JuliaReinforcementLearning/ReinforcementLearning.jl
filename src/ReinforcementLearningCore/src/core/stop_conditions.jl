4c387a75069194d0d1a1cedee15a995c5d489fd8
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-05-04 10:14:10 +0200

    Update src/ReinforcementLearningCore/src/core/stop_conditions.jl
    change docstring

--- src/ReinforcementLearningCore/src/core/stop_conditions.jl
+++ src/ReinforcementLearningCore/src/core/stop_conditions.jl
@@ -11,7 +11,7 @@ import ProgressMeter
 """
     ComposedStopCondition(stop_conditions...; reducer = any)
 
-The result of `stop_conditions` is reduced by `reducer`.
+The result of `stop_conditions` is reduced by `reducer`. The default `reducer` is the `any` function, which means that the condition is true when any one of the `stop_conditions...` is true. Can be replaced by any function returning a boolean. For example `reducer = x->sum(x) >= 2` will require at least two of the conditions to be true.
 """
 struct ComposedStopCondition{S,T}
     stop_conditions::S
