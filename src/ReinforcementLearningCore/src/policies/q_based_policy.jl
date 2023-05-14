59990f4b7337f4605f9d9ab3ca24ae454f0aba07
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-05-14 19:32:24 +0200

    Fix type signatures

--- src/ReinforcementLearningCore/src/policies/q_based_policy.jl
+++ src/ReinforcementLearningCore/src/policies/q_based_policy.jl
@@ -17,24 +17,24 @@ end
 
 @functor QBasedPolicy (learner,)
 
-function RLBase.plan!(p::QBasedPolicy{L,Ex}, env::E) where {L,Ex,E <: AbstractEnv}
+function RLBase.plan!(p::QBasedPolicy{L,Ex}, env::E) where {Ex<:AbstractExplorer,L<:AbstractLearner,E<:AbstractEnv}
     RLBase.plan!(p.explorer, p.learner, env)
 end
 
-function RLBase.plan!(explorer::Ex, learner::L, env::E) where {Ex,L,E<:AbstractEnv}
+function RLBase.plan!(explorer::Ex, learner::L, env::E) where {Ex<:AbstractExplorer,L<:AbstractLearner,E<:AbstractEnv}
     RLBase.plan!(explorer, estimate_reward(learner, env), legal_action_space_mask(env))
 end
 
-function RLBase.plan!(p::QBasedPolicy{L,Ex}, env::E, player::Symbol) where {L,Ex,E<:AbstractEnv}
+function RLBase.plan!(p::QBasedPolicy{L,Ex}, env::E, player::Symbol) where {Ex<:AbstractExplorer,L<:AbstractLearner,E<:AbstractEnv}
     RLBase.plan!(p.explorer, p.learner, env, player)
 end
 
-function RLBase.plan!(explorer::Ex, learner::L, env::E, player::Symbol) where {Ex,L,E<:AbstractEnv}
+function RLBase.plan!(explorer::Ex, learner::L, env::E, player::Symbol) where {Ex<:AbstractExplorer,L<:AbstractLearner,E<:AbstractEnv}
     legal_action_space_ = RLBase.legal_action_space_mask(env, player)
     return RLBase.plan!(p.explorer, estimate_reward(p.learner, env), legal_action_space_)
 end
 
-RLBase.prob(p::QBasedPolicy{L,Ex}, env::AbstractEnv) where {L,Ex} =
+RLBase.prob(p::QBasedPolicy{L,Ex}, env::AbstractEnv) where {L<:AbstractLearner,Ex<:AbstractExplorer} =
     prob(p.explorer, estimate_reward(p.learner, env), legal_action_space_mask(env))
 
-RLBase.optimise!(p::QBasedPolicy{L,Ex}, x::NamedTuple) where {L,Ex} = optimise!(p.learner, x)
+RLBase.optimise!(p::QBasedPolicy{L,Ex}, x::NamedTuple) where {L<:AbstractLearner,Ex<:AbstractExplorer} = optimise!(p.learner, x)
