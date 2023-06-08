1560ad1b8acbed34a3bfcad1f1cb12f062dad6ed
Cas Bex <cas.bex@kuleuven.be>
2023-06-08 09:59:02 +0200

    Refactor

--- src/ReinforcementLearningZoo/src/algorithms/dqns/NFQ.jl
+++ src/ReinforcementLearningZoo/src/algorithms/dqns/NFQ.jl
@@ -1,3 +1,8 @@
+export NFQ
+
+using Flux
+using Functors: @functor
+
 """
     NFQ{A<:AbstractApproximator, F, R} <: AbstractLearner
     NFQ(approximator::A, num_iterations::Integer epochs::Integer, loss_function::F, batch_size::Integer, rng::R, γ::Float32) where {A<:AbstractApproximator, F, R}
@@ -15,11 +20,11 @@ Neural Fitted Q-iteration as implemented in [1]
 # References
 [1] Riedmiller, M. (2005). Neural Fitted Q Iteration – First Experiences with a Data Efficient Neural Reinforcement Learning Method. In: Gama, J., Camacho, R., Brazdil, P.B., Jorge, A.M., Torgo, L. (eds) Machine Learning: ECML 2005. ECML 2005. Lecture Notes in Computer Science(), vol 3720. Springer, Berlin, Heidelberg. https://doi.org/10.1007/11564096_32
 """
-Base.@kwdef struct NFQ{A<:NeuralNetworkApproximator, F, R} <: AbstractLearner
+Base.@kwdef struct NFQ{A, R} <: AbstractLearner
     approximator::A
     num_iterations::Integer = 20
     epochs::Integer = 100
-    loss_function::F = mse
+    loss_function::Any = mse
     rng::R = Random.GLOBAL_RNG
     γ::Float32 = 0.9f0
 end
@@ -28,22 +33,26 @@ function NFQ(;
     approximator::A,
     num_iterations::Integer = 20,
     epochs::Integer = 1000,
-    loss_function::F = mse,
+    loss_function::Any = mse,
     rng=Random.GLOBAL_RNG,
     γ::Float32 = 0.9f0,
-    ) where {A<:NeuralNetworkApproximator, F}
+    ) where {A}
     NFQ(approximator, num_iterations, epochs, loss_function, rng, γ)
 end
 
 # Copied from BasicDQN but sure whether it's appropriate
-Flux.functor(x::NFQ) = (Q = x.approximator,), y -> begin
-    x = @set x.approximator = y.Q
-    x
-end
+@functor NFQ (approximator,)
 
-function RLBase.plan!(learner::NFQ, env::AbstractEnv)
+RLCore.forward(L::NFQ, s::AbstractArray) = RLCore.forward(L.approximator, s)
+
+# Flux.functor(x::NFQ) = (Q = x.approximator,), y -> begin
+#     x = @set x.approximator = y.Q
+#     x
+# end
+
+function RLCore.forward(learner::NFQ, env::AbstractEnv)
     as = action_space(env)
-    return vcat(repeat(state(env), inner=(1, length(as))), transpose(as)) |> x -> send_to_device(device(learner.approximator), x) |> learner.approximator |> send_to_host |> vec
+    return vcat(repeat(state(env), inner=(1, length(as))), transpose(as)) |> x -> send_to_device(device(learner.approximator), x) |> x->RLCore.forward(learner, x) |> send_to_host |> vec 
 end
 
 # Avoid optimisation in the middle of an episode
@@ -70,9 +79,9 @@ function _optimise!(learner::NFQ, batch::NamedTuple, env::AbstractEnv)
     s, a, r, ss = map(x->send_to_device(device(Q), x), (s, a, r, ss))
     for i = 1:learner.num_iterations
         # Make an input x samples x |action space| array -- Q --> samples x |action space| -- max --> samples
-        G = r .+ γ .* (cat(repeat(ss, inner=(1, 1, las)), reshape(repeat(as, outer=(1, size(ss, 2))), (1, size(ss, 2), las)), dims=1) |> Q |> x -> maximum(x, dims=3) |> vec)
+        G = r .+ γ .* (cat(repeat(ss, inner=(1, 1, las)), reshape(repeat(as, outer=(1, size(ss, 2))), (1, size(ss, 2), las)), dims=1) |> x -> maximum(RLCore.forward(Q, x), dims=3) |> vec)
         for e = 1:learner.epochs
-            Flux.train!((x, y) -> loss_func(Q(x), y), params(Q.model), [(vcat(s, transpose(a)), transpose(G))], Q.optimizer)
+            Flux.train!((x, y) -> loss_func(RLCore.forward(Q, x), y), params(Q.model), [(vcat(s, transpose(a)), transpose(G))], Q.optimiser)
         end
     end
 end
