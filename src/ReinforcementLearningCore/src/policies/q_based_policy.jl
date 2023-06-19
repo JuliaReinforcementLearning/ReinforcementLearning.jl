5542e35eab1725d204af413c67af3c855afa6916
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-06-19 12:25:47 +0200

    Add a docstring

--- src/ReinforcementLearningCore/src/policies/q_based_policy.jl
+++ src/ReinforcementLearningCore/src/policies/q_based_policy.jl
@@ -7,6 +7,11 @@ using Functors: @functor
 
 """
     QBasedPolicy(;learner, explorer)
+
+Wraps a learner and an explorer. The learner is a struct that should predict the Q-value of each legal
+action of an environment at its current state. It is typically a table or a neural network. 
+QBasedPolicy can be queried for an action with `RLBase.plan!`, the explorer will affect the action selection
+accordingly.
 """
 Base.@kwdef mutable struct QBasedPolicy{L,E} <: AbstractPolicy
     "estimate the Q value"
