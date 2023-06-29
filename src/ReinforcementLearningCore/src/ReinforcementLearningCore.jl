91d80e97af49518e7d1ec7218371e7b2c53510cb
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-06-29 16:35:53 +0200

    Add performance testing run loop (#914)

--- src/ReinforcementLearningCore/src/ReinforcementLearningCore.jl
+++ src/ReinforcementLearningCore/src/ReinforcementLearningCore.jl
@@ -1,5 +1,6 @@
 module ReinforcementLearningCore
 
+using TimerOutputs
 using ReinforcementLearningBase
 using Reexport
 
@@ -13,5 +14,8 @@ include("show.jl")
 include("core/core.jl")
 include("policies/policies.jl")
 include("utils/utils.jl")
+
+# Global timer for TimerOutputs.jl
+const timer = TimerOutput()
 
 end # module
