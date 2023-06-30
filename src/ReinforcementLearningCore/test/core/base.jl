4bff2c20e1812a4dfa97795bc39668e90e247bee
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-06-30 12:08:17 +0200

    Add debug test

--- src/ReinforcementLearningCore/test/core/base.jl
+++ src/ReinforcementLearningCore/test/core/base.jl
@@ -54,4 +54,23 @@ using ReinforcementLearningBase
             @test sum(hook[]) == length(agent.trajectory.container)
         end        
     end
+
+    @testset "Debug Timer" begin
+        RLCore.TimerOutputs.enable_debug_timings(RLCore)
+
+        env = RandomWalk1D()
+        agent = Agent(
+            RandomPolicy(legal_action_space(env)),
+            Trajectory(
+                CircularArraySARTTraces(; capacity = 1_000),
+                BatchSampler(1),
+                InsertSampleRatioController(n_inserted = -1),
+            ),
+            SRT{Any, Any, Any}(),
+        )            
+        stop_condition = StopAfterStep(123; is_show_progress=false)
+        hook = StepsPerEpisode()
+        run(agent, env, stop_condition, hook)
+        @test RLCore.timer isa TimerOutputs.TimerOutput
+    end
 end
