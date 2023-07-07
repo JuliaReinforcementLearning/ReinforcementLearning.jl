522da7722dc65a9056e0caee635c799eb0bbc127
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-07-07 14:34:15 +0200

    fix first tests

--- src/ReinforcementLearningCore/test/core/base.jl
+++ src/ReinforcementLearningCore/test/core/base.jl
@@ -1,4 +1,4 @@
-using ReinforcementLearningCore: SRT
+using ReinforcementLearningCore
 using ReinforcementLearningBase
 using TimerOutputs
 
@@ -18,7 +18,7 @@ using TimerOutputs
             hook = StepsPerEpisode()
             run(agent, env, stop_condition, hook)
 
-            @test sum(hook[]) == length(agent.trajectory.container)
+            @test sum(hook[]) + length(hook[]) - 1 == length(agent.trajectory.container)
         end
 
         @testset "StopAfterEpisode" begin
@@ -35,25 +35,8 @@ using TimerOutputs
             hook = StepsPerEpisode()
             run(agent, env, stop_condition, hook)
 
-            @test sum(hook[]) == length(agent.trajectory.container)
-        end
-
-        @testset "StopAfterStep, use type stable Agent" begin
-            env = RandomWalk1D()
-            agent = Agent(
-                RandomPolicy(legal_action_space(env)),
-                Trajectory(
-                    CircularArraySARTSTraces(; capacity = 1_000),
-                    BatchSampler(1),
-                    InsertSampleRatioController(n_inserted = -1),
-                ),
-                SRT{Any, Any, Any}(),
-            )            
-            stop_condition = StopAfterStep(123; is_show_progress=false)
-            hook = StepsPerEpisode()
-            run(agent, env, stop_condition, hook)
-            @test sum(hook[]) == length(agent.trajectory.container)
-        end        
+            @test length(hook[]) == 10
+        end      
     end
 
     @testset "Debug Timer" begin
@@ -66,8 +49,7 @@ using TimerOutputs
                 CircularArraySARTSTraces(; capacity = 1_000),
                 BatchSampler(1),
                 InsertSampleRatioController(n_inserted = -1),
-            ),
-            SRT{Any, Any, Any}(),
+            )
         )            
         stop_condition = StopAfterStep(123; is_show_progress=false)
         hook = StepsPerEpisode()
