48c5173b0a9e4c1489e1f477e7b9814874d31686
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-07-12 13:26:43 +0200

    change the MA loop

--- src/ReinforcementLearningCore/src/policies/agent/multi_agent.jl
+++ src/ReinforcementLearningCore/src/policies/agent/multi_agent.jl
@@ -125,18 +125,12 @@ function Base.run(
                 action = @timeit_debug timer "plan!"               RLBase.plan!(policy, env)
                 @timeit_debug timer "act!" act!(env, action)
 
-                
-
-                @timeit_debug timer "push!(policy) PostActStage"     push!(policy, PostActStage(), env)
+                @timeit_debug timer "push!(policy) PostActStage"     push!(policy, PostActStage(), env, action)
                 @timeit_debug timer "optimise! PostActStage"         optimise!(policy, PostActStage())
                 @timeit_debug timer "push!(hook) PostActStage"       push!(hook, PostActStage(), policy, env)
 
                 if check_stop(stop_condition, policy, env)
                     is_stop = true
-                    @timeit_debug timer "push!(policy) PreActStage"  push!(multiagent_policy, PreActStage(), env)
-                    @timeit_debug timer "optimise! PreActStage"      optimise!(multiagent_policy, PreActStage())
-                    @timeit_debug timer "push!(hook) PreActStage"    push!(multiagent_hook, PreActStage(), policy, env)
-                    @timeit_debug timer "plan!"                      RLBase.plan!(multiagent_policy, env)  # let the policy see the last observation
                     break
                 end
 
