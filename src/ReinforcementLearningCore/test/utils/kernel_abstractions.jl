903ef220e7242f1a09fd6ceb162d522a4d7df9ba
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-07-31 15:28:44 +0200

    Fix namespace

--- src/ReinforcementLearningCore/test/utils/kernel_abstractions.jl
+++ src/ReinforcementLearningCore/test/utils/kernel_abstractions.jl
@@ -1,17 +1,18 @@
-using KernelAbstractions: CPU, get_backend
+using KernelAbstractions
+using KernelAbstractions: CPU
 
-@testset "get_backend" begin
+@testset "KernelAbstractions.get_backend" begin
 
-    @test get_backend(rand(2)) == CPU(; static=false)
-    @test get_backend(Dense(2, 3)) == CPU(; static=false)
-    @test get_backend(Conv((2, 2), 1 => 16, relu)) == CPU(; static=false)
-    @test get_backend(Chain(x -> x .^ 2, Dense(2, 3))) == CPU(; static=false)
+    @test KernelAbstractions.get_backend(rand(2)) == CPU(; static=false)
+    @test KernelAbstractions.get_backend(Dense(2, 3)) == CPU(; static=false)
+    @test KernelAbstractions.get_backend(Conv((2, 2), 1 => 16, relu)) == CPU(; static=false)
+    @test KernelAbstractions.get_backend(Chain(x -> x .^ 2, Dense(2, 3))) == CPU(; static=false)
 
     if CUDA.functional()
-        @test get_backend(rand(2) |> gpu) isa CUDABackend
-        @test get_backend(Dense(2, 3) |> gpu) isa CUDABackend
-        @test get_backend(Conv((2, 2), 1 => 16, relu) |> gpu) isa CUDABackend
-        @test get_backend(Chain(x -> x .^ 2, Dense(2, 3)) |> gpu) isa CUDABackend
+        @test KernelAbstractions.get_backend(rand(2) |> gpu) isa CUDABackend
+        @test KernelAbstractions.get_backend(Dense(2, 3) |> gpu) isa CUDABackend
+        @test KernelAbstractions.get_backend(Conv((2, 2), 1 => 16, relu) |> gpu) isa CUDABackend
+        @test KernelAbstractions.get_backend(Chain(x -> x .^ 2, Dense(2, 3)) |> gpu) isa CUDABackend
     end
 
 end
