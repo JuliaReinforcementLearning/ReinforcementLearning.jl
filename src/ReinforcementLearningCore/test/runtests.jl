457cbc06d64883005f02eb42c795d03543ae2df4
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-08-02 11:16:59 +0200

    Set localprefs to metal for Flux on mac tests

--- src/ReinforcementLearningCore/test/runtests.jl
+++ src/ReinforcementLearningCore/test/runtests.jl
@@ -4,8 +4,14 @@ using ReinforcementLearningEnvironments
 using ReinforcementLearningTrajectories
 
 using Test
+using UUIDs
+using Preferences
+
 
 if Sys.isapple()
+    flux_uuid = UUID("587475ba-b771-5e3f-ad9e-33799f191a9c")
+    set_preferences!(flux_uuid, "gpu_backend" => "Metal")
+
     using Metal
 else
     using CUDA, cuDNN
@@ -13,6 +19,7 @@ end
 
 using CircularArrayBuffers
 using Flux
+println("Flux.GPU_BACKEND = $(Flux.GPU_BACKEND)")
 
 @testset "ReinforcementLearningCore.jl" begin
     include("core/core.jl")
--- src/ReinforcementLearningExperiments/test/runtests.jl
+++ src/ReinforcementLearningExperiments/test/runtests.jl
@@ -1,11 +1,19 @@
 using ReinforcementLearningExperiments
+using UUIDs
+using Preferences
 
 if Sys.isapple()
+    flux_uuid = UUID("587475ba-b771-5e3f-ad9e-33799f191a9c")
+    set_preferences!(flux_uuid, "gpu_backend" => "Metal")
+
     using Metal
 else
     using CUDA, cuDNN
     CUDA.allowscalar(false)
 end
+
+using Flux
+println("Flux.GPU_BACKEND = $(Flux.GPU_BACKEND)")
 
 run(E`JuliaRL_NFQ_CartPole`)
 run(E`JuliaRL_BasicDQN_CartPole`)
