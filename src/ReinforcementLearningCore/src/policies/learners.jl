13c9305362c51781cd32a31c08f50a898a83b64a
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-08-11 11:39:48 +0200

    move learner stuff to learners.jl

--- src/ReinforcementLearningCore/src/policies/learners.jl
+++ src/ReinforcementLearningCore/src/policies/learners.jl
@@ -5,12 +5,25 @@ using Functors: @functor
 
 abstract type AbstractLearner end
 
-Base.show(io::IO, m::MIME"text/plain", L::AbstractLearner) = show(io, m, convert(AnnotatedStructTree, L))
+function RLBase.plan!(explorer::AbstractExplorer, learner::AbstractLearner, env::AbstractEnv)
+    legal_action_space_ = RLBase.legal_action_space_mask(env)
+    RLBase.plan!(explorer, forward(learner, env), legal_action_space_)
+end
+
+function RLBase.plan!(explorer::AbstractExplorer, learner::AbstractLearner, env::AbstractEnv, player::Symbol)
+    legal_action_space_ = RLBase.legal_action_space_mask(env, player)
+    return RLBase.plan!(explorer, forward(learner, env), legal_action_space_)
+end
 
 # Take Learner and Environment, get state, send to RLCore.forward(Learner, State)
-forward(L::Le, env::E) where {Le <: AbstractLearner, E <: AbstractEnv} = env |> state |> send_to_device(L.approximator) |> x -> forward(L, x) |> send_to_device(env) 
+function forward(L::AbstractLearner, env::AbstractEnv)
+    s = state(env) |> send_to_device(L.approximator) 
+    forward(L,s) |> send_to_device(env) 
+end
 
 function RLBase.optimise!(::AbstractLearner, ::AbstractStage, ::Trajectory) end
+
+Base.show(io::IO, m::MIME"text/plain", L::AbstractLearner) = show(io, m, convert(AnnotatedStructTree, L))
 
 Base.@kwdef mutable struct Approximator{M,O}
     model::M
@@ -23,5 +36,4 @@ Base.show(io::IO, m::MIME"text/plain", A::Approximator) = show(io, m, convert(An
 
 forward(A::Approximator, args...; kwargs...) = A.model(args...; kwargs...)
 
-RLBase.optimise!(A::Approximator, gs) =
-    Flux.Optimise.update!(A.optimiser, Flux.params(A), gs)
+RLBase.optimise!(A::Approximator, gs) = Flux.Optimise.update!(A.optimiser, Flux.params(A), gs)
--- src/ReinforcementLearningCore/src/policies/q_based_policy.jl
+++ src/ReinforcementLearningCore/src/policies/q_based_policy.jl
@@ -26,17 +26,8 @@ function RLBase.plan!(p::QBasedPolicy{L,Ex}, env::E) where {Ex<:AbstractExplorer
     RLBase.plan!(p.explorer, p.learner, env)
 end
 
-function RLBase.plan!(explorer::Ex, learner::L, env::E) where {Ex<:AbstractExplorer,L<:AbstractLearner,E<:AbstractEnv}
-    RLBase.plan!(explorer, forward(learner, env), legal_action_space_mask(env))
-end
-
 function RLBase.plan!(p::QBasedPolicy{L,Ex}, env::E, player::Symbol) where {Ex<:AbstractExplorer,L<:AbstractLearner,E<:AbstractEnv}
     RLBase.plan!(p.explorer, p.learner, env, player)
-end
-
-function RLBase.plan!(explorer::Ex, learner::L, env::E, player::Symbol) where {Ex<:AbstractExplorer,L<:AbstractLearner,E<:AbstractEnv}
-    legal_action_space_ = RLBase.legal_action_space_mask(env, player)
-    return RLBase.plan!(explorer, forward(learner, env), legal_action_space_)
 end
 
 RLBase.prob(p::QBasedPolicy{L,Ex}, env::AbstractEnv) where {L<:AbstractLearner,Ex<:AbstractExplorer} =
