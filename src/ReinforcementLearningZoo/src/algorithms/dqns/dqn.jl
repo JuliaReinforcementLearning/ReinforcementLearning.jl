42104ccae9618f23bbabcffb6f52b730668f2160
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-08-11 16:21:02 +0200

    dqn

--- src/ReinforcementLearningZoo/src/algorithms/dqns/dqn.jl
+++ src/ReinforcementLearningZoo/src/algorithms/dqns/dqn.jl
@@ -3,20 +3,19 @@ export DQNLearner
 using Random: AbstractRNG
 using Functors: @functor
 
-Base.@kwdef mutable struct DQNLearner{A<:Approximator{<:TwinNetwork}, F, R} <: AbstractLearner
-    approximator::A
+Base.@kwdef mutable struct DQNLearner{A<:Union{Approximator,TargetNetwork}, F, R} <: AbstractLearner
+    q_network::A
     loss_func::F
     n::Int = 1
     γ::Float32 = 0.99f0
-    is_enable_double_DQN::Bool = true
     rng::R = Random.default_rng()
     # for logging
     loss::Float32 = 0.0f0
 end
 
-RLCore.forward(L::DQNLearner, s::A) where {A<:AbstractArray}  = RLCore.forward(L.approximator, s)
+RLCore.forward(L::DQNLearner, s::A) where {A<:AbstractArray}  = RLCore.forward(L.q_network, s)
 
-@functor DQNLearner (approximator,)
+@functor DQNLearner (q_network,)
 
 function RLCore.optimise!(learner::DQNLearner, ::PostActStage, trajectory::Trajectory)
     for batch in trajectory
@@ -25,10 +24,8 @@ function RLCore.optimise!(learner::DQNLearner, ::PostActStage, trajectory::Traje
 end
 
 function RLBase.optimise!(learner::DQNLearner, batch::NamedTuple)
-    A = learner.approximator
-    Q = A.model.source
-    Qₜ = A.model.target
-    @assert device(Q) == device(Qₜ) || @warn "Q and target Q function have to be on the same device"
+    Q = learner.q_network
+    Qt = target(learner.q_network)
     
     γ = learner.γ
     loss_func = learner.loss_func
@@ -38,18 +35,18 @@ function RLBase.optimise!(learner::DQNLearner, batch::NamedTuple)
     a = CartesianIndex.(a, 1:length(a))
     s, s_next, a, r, t = send_to_device(device(Q), (s, s_next, a, r, t))
 
-    q_next = learner.is_enable_double_DQN ? Q(s_next) : Qₜ(s_next)
+    q_next = forward(Qt, s_next)
 
     if haskey(batch, :next_legal_actions_mask)
         q_next .+= ifelse.(batch[:next_legal_actions_mask], 0.0f0, typemin(Float32))
     end
 
-    q_next_action = learner.is_enable_double_DQN ? Qₜ(s_next)[dropdims(argmax(q_next, dims=1), dims=1)] : dropdims(maximum(q_next; dims=1), dims=1)
+    q_next_action =  dropdims(maximum(q_next; dims=1), dims=1)
 
     R = r .+ γ^n .* (1 .- t) .* q_next_action
 
     gs = gradient(params(A)) do
-        qₐ = Q(s)[a]
+        qₐ = forward(Q, s)[a]
         loss = loss_func(R, qₐ)
         ignore_derivatives() do
             learner.loss = loss
