4a840c12c737ae95ac60dcce313a73f5b87d5f6c
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-09-18 11:33:23 +0200

    fix plan

--- src/ReinforcementLearningCore/src/policies/agent/agent_base.jl
+++ src/ReinforcementLearningCore/src/policies/agent/agent_base.jl
@@ -48,13 +48,12 @@ end
 # (partially), which will result to incorrect action. This should be addressed
 # in Oolong.jl with a wrapper
 @generated function RLBase.plan!(agent::Agent{<:Any, Trajectory{EpisodesBuffer{names}}}, env::AbstractEnv)
-    if :action_log_prob in names
+    if !(:action_log_prob in names)
         return :(RLBase.plan!(agent.policy, env))
     else
         return :(RLBase.plan!(agent.policy, env, is_return_log_prob = true)) #returns a tuple with the action and the log_prob.
         #Note: it is expected that a policy that needs is_return_log_prob will implement a plan! function that accepts the above arguments.
     end
-
 end
 
 function Base.push!(agent::Agent, ::PostActStage, env::AbstractEnv, action)
