0e54d98a250d5f95d0a54eb76c26d6ad3c70060b
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-10-10 11:30:39 +0200

    Update docstring

--- src/ReinforcementLearningCore/src/utils/networks.jl
+++ src/ReinforcementLearningCore/src/utils/networks.jl
@@ -114,12 +114,15 @@ end
 export SoftGaussianNetwork
 
 """
-    SoftGaussianNetwork(;pre=identity, μ, σ, min_σ=0f0, max_σ=Inf32)
+    SoftGaussianNetwork(;pre=identity, μ, σ, min_σ=0f0, max_σ=Inf32, squash = tanh)
 
-Returns `μ` and `σ` when called.  Create a distribution to sample from using
+Like `GaussianNetwork` but with a differentiable reparameterization trick. Mainly used for
+SAC. Returns `μ` and `σ` when called.  Create a distribution to sample from using
 `Normal.(μ, σ)`. `min_σ` and `max_σ` are used to clip the output from
 `σ`. `pre` is a shared body before the two heads of the NN. σ should be > 0. 
-You may enforce this using a `softplus` output activation. 
+You may enforce this using a `softplus` output activation. The `squash` function is
+applied elementwise to the action. If squash is `tanh`, a correction is applied to the
+logpdf.
 """
 Base.@kwdef struct SoftGaussianNetwork{P,U,S,F}
     pre::P = identity
