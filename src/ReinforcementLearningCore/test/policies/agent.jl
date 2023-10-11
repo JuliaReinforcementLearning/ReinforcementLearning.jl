698fd4ba2c241e8ed829b1044dd167ab5548a633
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-10-11 15:13:44 +0200

    remove rlenv dep for tests

--- src/ReinforcementLearningCore/test/policies/agent.jl
+++ src/ReinforcementLearningCore/test/policies/agent.jl
@@ -1,6 +1,5 @@
-using ReinforcementLearningBase, ReinforcementLearningEnvironments
-using ReinforcementLearningCore: SRT
-using ReinforcementLearningCore
+using ReinforcementLearningBase
+import ReinforcementLearningCore.SRT
 
 @testset "agent.jl" begin
     @testset "Agent Tests" begin
