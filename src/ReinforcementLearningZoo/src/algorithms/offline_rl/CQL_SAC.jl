03e9d0da95db176294a70d2fd95da12646f7eef8
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2023-10-20 15:56:15 +0200

    write doc

--- src/ReinforcementLearningZoo/src/algorithms/offline_rl/CQL_SAC.jl
+++ src/ReinforcementLearningZoo/src/algorithms/offline_rl/CQL_SAC.jl
@@ -1,6 +1,32 @@
 import LogExpFunctions
 export CQLSACPolicy
 
+"""
+    CQLSACPolicy(
+        sac::SACPolicy,
+        action_sample_size::Int = 10,
+        α_cql::Float32 = 0f0,
+        α_lr::Float32 = 1f-3,
+        τ_cql::Float32 = 5f0,
+        α_cql_autotune::Bool = true,
+        cons_weight::Float32 = 1f0, #multiplies the Q difference before substracting tau, used to scale with the rewards.
+        finetune_experiment::E = nothing #Provide an second experiment to run at PostExperimentStage to finetune the sac policy, typically with an agent that uses the sac policy. Leave nothing if no finetuning is desired.
+    )
+
+    Implements the Conservative Q-Learning algorithm [1] in its continuous variant on top of the SAC algorithm [2]. CQLSACPolicy wraps a classic SACPolicy whose networks will be trained normally, except for the additional conservative loss.
+    CQLSACPolicy contains the additional hyperparameters that are specific to this method. α_cql is the lagrange penalty for the conservative_loss, it will be automatically tuned if ` α_cql_autotune = true`. `cons_weight` is a scaling parameter 
+    which may be necessary to decrease if the scale of the Q-values is large. `τ_cql` is the threshold of the lagrange conservative penalty.
+    See SACPolicy for all the other hyperparameters related to SAC.
+
+    If desired, you can provide an `Experiment(agent, env, stop_condition, hook)` to finetune_experiment to finish the training with a finetuning run. `agent` should be a normal `Agent` with policy being `sac`, an environment to finetune on. 
+    See the example in ReinforcementLearningExperiments.jl for an example on the Pendulum task.
+    
+    As this is an offline algorithm, it must be wrapped in an `OfflineAgent` which will not update the trajectory as the training progresses. However, it _will_ interact with the supplied environment, which may be useful to record the progress.
+    This can be avoided by supplying a dummy environment. 
+
+    [1] Kumar, A., Zhou, A., Tucker, G., & Levine, S. (2020). Conservative q-learning for offline reinforcement learning. Advances in Neural Information Processing Systems, 33, 1179-1191.
+    [2] Haarnoja, T. et al. (2018). Soft actor-critic algorithms and applications. arXiv preprint arXiv:1812.05905.
+"""
 Base.@kwdef mutable struct CQLSACPolicy{P<:SACPolicy, E<:Union{Experiment,Nothing}} <: AbstractPolicy
     sac::P
     action_sample_size::Int = 10
