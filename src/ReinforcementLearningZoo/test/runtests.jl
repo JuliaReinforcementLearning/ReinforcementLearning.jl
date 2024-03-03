70e0c0c37559417d647c9328e560d051acc99cbe
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-03-03 19:28:12 +0100

    Fix RLZoo compat

--- src/ReinforcementLearningZoo/test/runtests.jl
+++ src/ReinforcementLearningZoo/test/runtests.jl
@@ -1,4 +1,15 @@
-using Test
+using UUIDs
+using Preferences
+
+if Sys.isapple()
+    flux_uuid = UUID("587475ba-b771-5e3f-ad9e-33799f191a9c")
+    set_preferences!(flux_uuid, "gpu_backend" => "Metal")
+
+    using Metal
+else
+    using CUDA, cuDNN
+    CUDA.allowscalar(false)
+end
 
 @testset "ReinforcementLearningZoo.jl" begin
     # include("cfr/cfr.jl")
