b96316ff3f94097ba32985c5eb852d9e3c1764a1
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-03-03 18:44:46 +0100

    Apple silicon check

--- src/ReinforcementLearningExperiments/test/runtests.jl
+++ src/ReinforcementLearningExperiments/test/runtests.jl
@@ -1,7 +1,7 @@
 using UUIDs
 using Preferences
 
-if Sys.isapple()
+if Sys.isapple() && Sys.ARCH === :aarch64
     flux_uuid = UUID("587475ba-b771-5e3f-ad9e-33799f191a9c")
     set_preferences!(flux_uuid, "gpu_backend" => "Metal")
 
