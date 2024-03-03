56056538d7cff552b69ee951900bd134218c382f
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-03-03 09:51:56 +0100

    Limit duplicate warnings

--- src/ReinforcementLearningBase/src/space.jl
+++ src/ReinforcementLearningBase/src/space.jl
@@ -62,7 +62,7 @@ function ==(a1::NamedTupleProductDomain, a2::NamedTupleProductDomain)
     try
         return isequaldomain(a1, a2)
     catch e
-        @warn "Use StaticArrays (e.g. SVector, SMatrix, or SArray when constructing `NamedTupleProductDomain` objects."
+        @warn "Use StaticArrays (e.g. SVector, SMatrix, or SArray when constructing `NamedTupleProductDomain` objects." maxlog=1
         return typeof(a1) == typeof(a2)
     end
 end
