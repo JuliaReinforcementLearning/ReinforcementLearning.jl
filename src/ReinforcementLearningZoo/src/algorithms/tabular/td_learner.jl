1fdb996bdc97d141a14cd2b5952c4e5d4ce11e1e
Jeremiah Lewis <--get>
2024-03-08 18:05:39 +0100

    add tests

--- src/ReinforcementLearningZoo/src/algorithms/tabular/td_learner.jl
+++ src/ReinforcementLearningZoo/src/algorithms/tabular/td_learner.jl
@@ -52,7 +52,7 @@ function Q!(
 ) where {I1<:Integer,I2<:Integer,I3<:Integer,F1<:AbstractFloat,F2<:AbstractFloat}
     # Q-learning formula according to https://github.com/JuliaPOMDP/TabularTDLearning.jl/blob/25c4d3888e178c51ed1ff448f36b0fcaf7c1d8e8/src/q_learn.jl#LL63C26-L63C95
     q_value_updated = α * (π_ + γ * maximum(Q(app, s_plus_one)) - Q(app, s, a))
-    app.table[a, s] += q_value_updated
+    app.model[a, s] += q_value_updated
     return Q(app, s, a)
 end
 
--- src/ReinforcementLearningZoo/test/algorithms/tabular/td_learner.jl
+++ src/ReinforcementLearningZoo/test/algorithms/tabular/td_learner.jl
@@ -12,3 +12,43 @@ using Test
     approximator = TabularQApproximator(n_state=5, n_action=3)
     @test TDLearner(approximator, :SARS, γ=0.95, n=0) isa TDLearner
 end
+
+# Test TDLearner struct
+@testset "TDLearner struct" begin
+    approximator = TabularQApproximator(n_state=5, n_action=3)
+    learner = TDLearner(approximator, :SARS)
+    @test learner.approximator === approximator
+    @test learner.γ == 1.0
+    @test learner.n == 0
+end
+
+# Test Q! function
+@testset "Q! function" begin
+    approximator = TabularQApproximator(n_state=5, n_action=3)
+    s = 1
+    s_plus_one = 2
+    a = 3
+    α = 0.1
+    π_ = 0.5
+    γ = 0.9
+    RLZoo.Q!(approximator, s, s_plus_one, a, α, π_, γ)
+    @test RLZoo.Q(approximator, s, a) ≈ α * (π_ + γ * maximum(Q(approximator, s_plus_one)) - Q(approximator, s, a))
+end
+
+# Test optimise! function
+@testset "optimise! function" begin
+    approximator = TabularQApproximator(n_state=5, n_action=3)
+    learner = TDLearner(approximator, :SARS)
+    t = (state=1, next_state=2, action=3, reward=0.5, terminal=false)
+    optimise!(learner, t)
+    @test true  # Add your own assertion here
+end
+
+# Test priority function
+@testset "priority function" begin
+    approximator = TabularQApproximator(n_state=5, n_action=3)
+    learner = TDLearner(approximator, :SARS)
+    transition = (1, 2, 0.5, false, 3)
+    priority(learner, transition)
+    @test true  # Add your own assertion here
+end
