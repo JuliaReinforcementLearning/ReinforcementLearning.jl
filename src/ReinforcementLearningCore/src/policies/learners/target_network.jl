b2202cce4b286f29095ea6d71516e57646cf92e8
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-03-12 16:13:14 +0100

    Update src/ReinforcementLearningCore/src/policies/learners/target_network.jl
    Co-authored-by: Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>

--- src/ReinforcementLearningCore/src/policies/learners/target_network.jl
+++ src/ReinforcementLearningCore/src/policies/learners/target_network.jl
@@ -41,7 +41,7 @@ Constructs a target network for reinforcement learning.
 # Arguments
 - `network`: The main network used for training.
 - `sync_freq`: The frequency (in number of calls to `optimise!`) at which the target network is synchronized with the main network. Default is 1.
-- `ρ`: The interpolation factor used for updating the target network. Must be in the range [0, 1]. Default is 0.
+- `ρ`: The interpolation factor used for updating the target network. Must be in the range [0, 1]. Default is 0 (the old weights are completely replaced by the new ones).
 - `use_gpu`: Specifies whether to use GPU for the target network. Default is `false`.
 
 # Returns
