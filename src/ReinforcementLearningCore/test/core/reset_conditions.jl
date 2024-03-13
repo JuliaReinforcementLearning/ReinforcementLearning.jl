b2dd6261dfb51fbe05623323212b2092db3c00de
Jeremiah Lewis <--get>
2024-03-13 18:14:54 +0100

    fix reset tests

--- src/ReinforcementLearningCore/test/core/reset_conditions.jl
+++ src/ReinforcementLearningCore/test/core/reset_conditions.jl
@@ -1,22 +1,24 @@
 using Test
 using ReinforcementLearningCore
+
 @testset "Reset Conditions Tests" begin
 
     @testset "Test ResetIfEnvTerminated" begin
         policy = RandomPolicy()
         env = RandomWalk1D()
         reset_condition = ResetIfEnvTerminated()
-        is_terminated(env) = true
-        @test check!(reset_condition, policy, env) == true
+        env.pos = 1
+        @test RLCore.check!(reset_condition, policy, env) == true
     end
 
     @testset "Test ResetAfterNSteps" begin
         policy = RandomPolicy()
         env = RandomWalk1D()
         reset_condition = ResetAfterNSteps(3)
-        @test check!(reset_condition, policy, env) == false
-        @test check!(reset_condition, policy, env) == false
-        @test check!(reset_condition, policy, env) == true
-        @test check!(reset_condition, policy, env) == false
+        @test RLCore.check!(reset_condition, policy, env) == false
+        @test RLCore.check!(reset_condition, policy, env) == false
+        @test RLCore.check!(reset_condition, policy, env) == false
+        @test RLCore.check!(reset_condition, policy, env) == true
+        @test RLCore.check!(reset_condition, policy, env) == false
     end
 end
