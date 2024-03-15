d002258e2dd65a0ccab6236772142d7ecabb0db2
Jeremiah Lewis <--get>
2024-03-15 17:28:19 +0100

    qlearning passes first check

--- src/ReinforcementLearningFarm/src/algorithms/tabular/td_learner.jl
+++ src/ReinforcementLearningFarm/src/algorithms/tabular/td_learner.jl
@@ -5,6 +5,7 @@ using Distributions: pdf
 import Base.push!
 
 using ReinforcementLearningCore: AbstractLearner, TabularApproximator
+using Flux
 
 """
     TDLearner(;approximator, γ=1.0, method, n=0)
@@ -43,31 +44,33 @@ Q(app::TabularApproximator, s) = RLCore.forward(app, s)
 Update the Q-value of the given state-action pair.
 """
 function Q!(
-    app::TabularApproximator,
+    approx::TabularApproximator,
     s::I1,
     s_plus_one::I2,
     a::I3,
-    α::F1,
-    π_::F2,
-    γ::Float64,
-) where {I1<:Integer,I2<:Integer,I3<:Integer,F1<:AbstractFloat,F2<:AbstractFloat}
+    π_::F1,
+    γ::Float64, # discount factor
+) where {I1<:Integer,I2<:Integer,I3<:Integer,F1<:AbstractFloat}
     # Q-learning formula following https://github.com/JuliaPOMDP/TabularTDLearning.jl/blob/25c4d3888e178c51ed1ff448f36b0fcaf7c1d8e8/src/q_learn.jl#LL63C26-L63C95
-    q_value_updated = α * (π_ + γ * maximum(Q(app, s_plus_one)) - Q(app, s, a))
-    app.model[a, s] += q_value_updated
-    return Q(app, s, a)
+    # Terminology following https://en.wikipedia.org/wiki/Q-learning
+    estimate_optimal_future_value = maximum(Q(approx, s_plus_one))
+    current_value = Q(approx, s, a)
+    raw_q_value = (π_ + γ * estimate_optimal_future_value - current_value) # Discount factor γ is applied here
+    q_value_updated = Flux.Optimise.apply!(approx.optimiser_state, :learning, [raw_q_value])[] # adust according to optimiser learning rate
+    approx.model[a, s] += q_value_updated
+    return Q(approx, s, a)
 end
 
 function _optimise!(
     n::I1,
     γ::F,
-    app::Approximator{Ar},
+    approx::Approximator{Ar},
     s::I2,
     s_next::I2,
     a::I3,
     r::F,
 ) where {I1<:Number,I2<:Number,I3<:Number,Ar<:AbstractArray,F<:AbstractFloat}
-    α = app.optimiser_state.eta
-    Q!(app, s, s_next, a, α, r, γ)
+    Q!(approx, s, s_next, a, r, γ)
 end
 
 function RLBase.optimise!(
--- src/ReinforcementLearningFarm/test/algorithms/tabular/td_learner.jl
+++ src/ReinforcementLearningFarm/test/algorithms/tabular/td_learner.jl
@@ -20,15 +20,21 @@ end
 
 # Test Q! function
 @testset "Q! function" begin
-    approximator = TabularQApproximator(n_state=5, n_action=3)
+    approximator = TabularQApproximator(n_state=5, n_action=3, opt = InvDecay(0.7))
     s = 1
     s_plus_one = 2
     a = 3
-    α = 0.1
-    π_ = 0.5
+    α = 1/(1 + approximator.optimiser_state.gamma)
+    π_ = 5.0
     γ = 0.9
-    RLFarm.Q!(approximator, s, s_plus_one, a, α, π_, γ)
-    @test RLFarm.Q(approximator, s, a) ≈ α * (π_ + γ * maximum(RLFarm.Q(approximator, s_plus_one)) - RLFarm.Q(approximator, s, a))
+    approximator.model[2, s_plus_one] = 15
+    approximator.model[a, s] = 2
+
+    # Following https://en.wikipedia.org/wiki/Q-learning#Algorithm
+    q_should_be = (1-α) * RLFarm.Q(approximator, s, a) + α * (π_ + γ * maximum(RLFarm.Q(approximator, s_plus_one)))
+
+    @test RLFarm.Q!(approximator, s, s_plus_one, a, π_, γ) ≈ q_should_be
+    @test RLFarm.Q(approximator, s, a) ≈ q_should_be
 end
 
 # Test optimise! function
