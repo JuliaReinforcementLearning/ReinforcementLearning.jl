fef5e44018bd11d892de8a1bbaba4ff275e1a607
Jeremiah Lewis <--get>
2024-03-20 10:25:12 +0100

    Update FluxModelApproximator references to FluxApproximator

---
+++ src/ReinforcementLearningCore/src/policies/learners/flux_approximator.jl
@@ -0,0 +1,47 @@
+export FluxApproximator
+
+using Flux
+
+"""
+    FluxApproximator(model, optimiser)
+
+Wraps a Flux trainable model and implements the `RLBase.optimise!(::FluxApproximator, ::Gradient)` 
+interface. See the RLCore documentation for more information on proper usage.
+"""
+struct FluxApproximator{M,O} <: AbstractLearner
+    model::M
+    optimiser_state::O
+end
+
+
+"""
+    FluxApproximator(; model, optimiser, usegpu=false)
+
+Constructs an `FluxApproximator` object for reinforcement learning.
+
+# Arguments
+- `model`: The model used for approximation.
+- `optimiser`: The optimizer used for updating the model.
+- `usegpu`: A boolean indicating whether to use GPU for computation. Default is `false`.
+
+# Returns
+An `FluxApproximator` object.
+"""
+function FluxApproximator(; model, optimiser, use_gpu=false)
+    optimiser_state = Flux.setup(optimiser, model)
+    if use_gpu  # Pass model to GPU (if available) upon creation
+        return FluxApproximator(gpu(model), gpu(optimiser_state))
+    else
+        return FluxApproximator(model, optimiser_state)
+    end
+end
+
+FluxApproximator(model, optimiser::Flux.Optimise.AbstractOptimiser; use_gpu=false) = FluxApproximator(model=model, optimiser=optimiser, use_gpu=use_gpu)
+
+Flux.@layer FluxApproximator trainable=(model,)
+
+forward(A::FluxApproximator, args...; kwargs...) = A.model(args...; kwargs...)
+forward(A::FluxApproximator, env::E) where {E <: AbstractEnv} = env |> state |> (x -> forward(A, x))
+
+RLBase.optimise!(A::FluxApproximator, grad::NamedTuple) =
+    Flux.Optimise.update!(A.optimiser_state, A.model, grad.model)
--- src/ReinforcementLearningCore/test/policies/learners/flux_approximator.jl
+++ src/ReinforcementLearningCore/test/policies/learners/flux_approximator.jl
@@ -1,13 +1,13 @@
 using Test
 using Flux
 
-@testset "FluxModelApproximator Tests" begin
+@testset "FluxApproximator Tests" begin
     @testset "Creation, with use_gpu = true toggle" begin
         model = Chain(Dense(10, 5, relu), Dense(5, 2))
         optimiser = Adam()
-        approximator = FluxModelApproximator(model=model, optimiser=optimiser, use_gpu=true)
+        approximator = FluxApproximator(model=model, optimiser=optimiser, use_gpu=true)
 
-        @test approximator isa FluxModelApproximator
+        @test approximator isa FluxApproximator
         @test typeof(approximator.model) == typeof(gpu(model))
         @test approximator.optimiser_state isa NamedTuple
     end
@@ -15,7 +15,7 @@ using Flux
     @testset "Forward" begin
         model = Chain(Dense(10, 5, relu), Dense(5, 2))
         optimiser = Adam()
-        approximator = FluxModelApproximator(model=model, optimiser=optimiser, use_gpu=false)
+        approximator = FluxApproximator(model=model, optimiser=optimiser, use_gpu=false)
 
         input = rand(Float32, 10)
         output = RLCore.forward(approximator, input)
@@ -27,7 +27,7 @@ using Flux
     @testset "Forward to environment" begin
         model = Chain(Dense(4, 5, relu), Dense(5, 2))
         optimiser = Adam()
-        approximator = FluxModelApproximator(model=model, optimiser=optimiser, use_gpu=false)
+        approximator = FluxApproximator(model=model, optimiser=optimiser, use_gpu=false)
 
         env = CartPoleEnv(T=Float32)
         output = RLCore.forward(approximator, env)
@@ -38,7 +38,7 @@ using Flux
     @testset "Optimise" begin
         model = Chain(Dense(10, 5, relu), Dense(5, 2))
         optimiser = Adam()
-        approximator = FluxModelApproximator(model=model, optimiser=optimiser)
+        approximator = FluxApproximator(model=model, optimiser=optimiser)
 
         input = rand(Float32, 10)
         
