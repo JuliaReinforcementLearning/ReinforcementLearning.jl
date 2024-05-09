ce7415f2a7d1477c6950367304c9a570e03beb57
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-09 07:13:20 +0000

    Fix testset call

--- src/ReinforcementLearningEnvironments/test/environments/examples/stock_trading_env.jl
+++ src/ReinforcementLearningEnvironments/test/environments/examples/stock_trading_env.jl
@@ -6,7 +6,7 @@
     RLBase.test_runnable!(env)
 end
 
-@test "StockTradingEnv legal_action_space_mask" begin
+@testset "StockTradingEnv legal_action_space_mask" begin
     env = StockTradingEnv()
     # For MINIMAL_ACTION_SET, this is not and should not be defined
     @test_throws MethodError legal_action_space_mask(env) 
